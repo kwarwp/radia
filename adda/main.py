a4355e9ad4ef94ef98cf7477f36c740e621162b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-12 13:17:52 -0300

    Created adda/main.py @2023-09-12 16:17

---
+++ adda/main.py
@@ -0,0 +1,7 @@
+# radia.adda.main.py
+#_author_Glaucio
+from _spy.vitollino.main import Cena, Elemento
+IMAGE = "https://www.comciencia.br/wp-content/uploads/2021/09/oceano2-672x372.jpg"
+PORTAO_BRONZE = "https://img.freepik.com/fotos-premium/alivio-com-leao-no-portao-de-bronze-ornamentado-na-entrada-do-castelo-hohenzollern-na-alemanha_788189-3033.jpg" 
+oceano = Cena(IMAGE).vai()
+#portao = Elemento(PORTAO_BRONZE, x= 10, y= 10, w= 100, h= 100, cena=oceano)
