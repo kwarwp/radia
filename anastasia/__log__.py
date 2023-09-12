76a606fdf59c1fbbd1339d2692f669c200d4bf60
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-12 12:59:45 -0300

    Created anastasia/__log__.py @2023-09-12 15:59

---
+++ anastasia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 12 2023 12:59:44.1 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  from _spy.vitolino.main  
+                           ^
+SyntaxError: invalid syntax
+'''},
