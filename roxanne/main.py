76e8212bf0dbc874fbbe171bc1b4ae0f860a687a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-12 13:13:06 -0300

    Saved roxanne/main.py @2023-09-12 16:13

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,2 +1,8 @@
 # radia.roxanne.main.py
 # __author__ Carlo
+from _spy.vitollino.main import Cena, Elemento
+IMAGEM = 'https://www.comciencia.br/wp-content/uploads/2021/09/oceano2-672x372.jpg'
+PORTAO_BRONZE= "https://img.freepik.com/fotos-premium/fragmento-de-portao-de-bronze-de-ferro-forjado_172420-936.jpg?w=2000"
+oceano = Cena(IMAGEM).vai()
+portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
+
