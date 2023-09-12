0a6e88d1e0c8d6f16e705a558ab21aa60edf0cd2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-12 13:05:35 -0300

    Saved anastasia/main.py @2023-09-12 16:05

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,6 +1,6 @@
 # radia.anastasia.main.py
 #_author_vanessa
-print("alo")
+print("alo")#
 from _spy.vitollino.main import Cena 
 IMAGEM = 'https://www.mafiadomergulho.com.br/wp-content/uploads/2021/04/fundo-do-mar-site-copy.jpg'#COLOCAR IMAGEM DA INTERNET 
 Cena(IMAGEM).vai
