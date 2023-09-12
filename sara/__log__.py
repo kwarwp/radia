c913d46e66b35e39cc9a15b6fd716100b3ebb105
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-12 13:16:13 -0300

    Saved sara/__log__.py @2023-09-12 16:16

--- sara/__log__.py
+++ sara/__log__.py
@@ -52,3 +52,19 @@ NameError: name 'Elemento' is not defined
     portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano) 
 NameError: name 'Elemento' is not defined
 '''},
+{'date': 'Tue Sep 12 2023 12:15:26.983 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano) 
+NameError: name 'Elemento' is not defined
+'''},
