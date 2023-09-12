6e57b0b07945e491bb3cf322d67c6bff7c4f5cc3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-12 13:18:52 -0300

    Saved anastasia/__log__.py @2023-09-12 16:18

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -6,3 +6,10 @@
                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 12 2023 13:18:52.712 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100 cena=oceano)
+                                                             ^
+SyntaxError: invalid syntax
+'''},
