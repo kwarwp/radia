854685039f5cb38a5ec3f716593a1c38f820ddd9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-12 13:15:19 -0300

    Saved sara/main.py @2023-09-12 16:15

--- sara/main.py
+++ sara/main.py
@@ -1,2 +1,9 @@
 # radia.sara.main.py
 # Fernanda - POO 2023 - UFRJ
+from _spy.vitollino.main import Cena 
+IMAGEM = 'https://dinamicambiental.com.br/wp-content/uploads/2021/06/o-oceano-e-a-nossa-sobrevivencia.jpg'
+PORTAO_BRONZE = 'https://thumbs.dreamstime.com/z/batedor-de-bronze-em-forma-uma-cabe%C3%A7a-le%C3%A3o-do-port%C3%A3o-da-catedral-col%C3%B4nia-na-alemanha-194660830.jpg?w=992'
+Cena(IMAGEM).vai()
+Oceano = Cena(IMAGEM).vai()
+portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano) 
+
