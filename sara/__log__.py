cea421694ad35ec1df69a0805fbc7bb03c79e08a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-12 13:05:24 -0300

    Saved sara/__log__.py @2023-09-12 16:05

--- sara/__log__.py
+++ sara/__log__.py
@@ -6,3 +6,10 @@
                                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 12 2023 12:04:38.318 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  from _spy.vitollino.main import Cena IMAGEM = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Folhardigital.com.br%2F2023%2F07%2F17%2Fciencia-e-espaco%2Fo-que-ha-abaixo-do-fundo-do-oceano%2F&psig=AOvVaw2M0xlj16-rLJdwun5ANJKj&ust=1694621015392000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCNCOpeerpYEDFQAAAAAdAAAAABAD'
+                                               ^
+SyntaxError: invalid syntax
+'''},
