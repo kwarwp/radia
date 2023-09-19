54906678fad704521ae6d3e90a3e00fb49925612
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:58:29 -0300

    Created grace/__log__.py @2023-09-19 14:58

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 19 2023 11:58:27.168 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  protao = Elemento(PORTAO_BRONZE), x=10, y=10, w=100, h=100, cena=oceano)
+                                                                    ^
+SyntaxError: invalid syntax
+'''},
