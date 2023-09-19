5d94c809bb252469f5f1c084850d3d6e7917f604
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:29:53 -0300

    Saved anastasia/__log__.py @2023-09-19 15:29

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -143,3 +143,20 @@ NameError: name 'STYLE' is not defined
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Sep 19 2023 11:46:51.589 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''alo
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    STYLE["width"] = 800 #a varios estilos, aqui selecionamos o widht 
+NameError: name 'STYLE' is not defined
+'''},
