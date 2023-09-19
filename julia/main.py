54c7f1869fb013317cbe25e24fef9492557fc09f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:04:12 -0300

    Saved julia/main.py @2023-09-19 15:04

--- julia/main.py
+++ julia/main.py
@@ -3,7 +3,7 @@
 from _spy.vitollino.main import Cena, Elemento
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
 PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
-PALACIO_CORAL = "https://imgur.com/tLDbzD2.jpg"
+PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
 
 class IlhaProibida:
 	def __init__(self):
