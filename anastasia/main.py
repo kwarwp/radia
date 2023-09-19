940ae177658c62ec0f6551d8b2849899c4456505
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:01:28 -0300

    Saved anastasia/main.py @2023-09-19 15:01

--- anastasia/main.py
+++ anastasia/main.py
@@ -6,9 +6,13 @@ IMAGEM = "https://www.mafiadomergulho.com.br/wp-content/uploads/2021/04/fundo-do
 PORTAO_BRONZE = "https://imgur.com/bl61b7H.jpg"
 PALACIO_CORAL = "https://imgur.com/bl61b7H.jpg"
 
+
+
 class IlhaProibida:  # : significa inicio de um bloco então posteriormente tem que estar intendado
     def __init__(self): #construcao, o primeiro parametro sempre se chama self
         oceano = Cena(IMAGEM).vai()
-        portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
-        palacio = Elemento(PALACIO_CORAL, X=20, Y=20, W=100, H=100, cena=oceano)
+        portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, tit="Portão de Bronze", cena=oceano)
+        palacio = Elemento(PALACIO_CORAL, x=30, y=10, w=100, h=100, tit="Palacio coral", cena=oceano)
+        
+        
 IlhaProibida()
