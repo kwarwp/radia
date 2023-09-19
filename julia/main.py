007257551fdae07e6a4cd3f7e8517a9adff8a294
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:52:06 -0300

    Saved julia/main.py @2023-09-19 14:52

--- julia/main.py
+++ julia/main.py
@@ -7,6 +7,6 @@ PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
 class IlhaProibida:
 	def __init__(self):
     	oceano = Cena(IMAGEM).vai()
-          portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=1--, cena=oceano)
+        portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=1--, cena=oceano)
           
 IlhaProibida()
