9427a4bad9fca5d446984f5c72626c194ebd86a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:09:29 -0300

    Saved grace/main.py @2023-09-19 15:09

--- grace/main.py
+++ grace/main.py
@@ -1,14 +1,4 @@
 # radia.grace.main.py
 # __author__ Finn
-from _spy.vitollino.main import Cena, Elemento
 
-IMAGEM = "https://imgur.com/a/cJCkxeA"
-PORTAO_BRONZE = "https://imgur.com/103Lydz"
-
-class IlhaProibida:
-    def __init__(self):
-        oceano = Cena(IMAGEM).vai()
-        portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
-        
-IlhaProibida()
 print("test")
