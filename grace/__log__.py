8a91d2f2be6aae44eb7f715924ec2cbf5681ee98
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:28:57 -0300

    Saved grace/__log__.py @2023-09-19 15:28

--- grace/__log__.py
+++ grace/__log__.py
@@ -49,3 +49,21 @@ NameError: name 'PORTAO_BRONZE' is not defined
                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 19 2023 12:28:57.39 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    Peao()
+  module <module> line 24
+    pawn = Elemento(PAWN, x=10, y=10, w=100, h=100, cena=oceano)
+NameError: name 'oceano' is not defined
+'''},
