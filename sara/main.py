60702994e68d1e8870845894d23452753095b771
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:51:40 -0300

    Saved sara/main.py @2023-09-19 14:51

--- sara/main.py
+++ sara/main.py
@@ -3,7 +3,12 @@
 from _spy.vitollino.main import Cena, Elemento
 IMAGEM = 'https://dinamicambiental.com.br/wp-content/uploads/2021/06/o-oceano-e-a-nossa-sobrevivencia.jpg'
 PORTAO_BRONZE = 'https://thumbs.dreamstime.com/z/batedor-de-bronze-em-forma-uma-cabe%C3%A7a-le%C3%A3o-do-port%C3%A3o-da-catedral-col%C3%B4nia-na-alemanha-194660830.jpg?w=992'
-#Cena(IMAGEM).vai()
-oceano = Cena(IMAGEM).vai()
-portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano) 
+#Cena é classe e cena é atributo
 
+class IlhaProibida:
+    def __init__(self):
+        oceano = Cena(IMAGEM).vai()
+        portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
+
+
+IlhaProibida()
