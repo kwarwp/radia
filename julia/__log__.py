5533ca0345dfbc780ec5b12119779126b88f5313
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:51:36 -0300

    Created julia/__log__.py @2023-09-19 14:51

---
+++ julia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 19 2023 11:51:34.800 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  oceano = Cena(IMAGEM).vai()
+  ^
+IndentationError: expected an indented block
+'''},
