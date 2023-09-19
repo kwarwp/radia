b5f554e15624817a4015f940295b4ef9728afc7d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:02:50 -0300

    Saved julia/main.py @2023-09-19 15:02

--- julia/main.py
+++ julia/main.py
@@ -7,9 +7,9 @@ PALACIO_CORAL = "https://imgur.com/tlDbzD2.jpg"
 
 class IlhaProibida:
 	def __init__(self):
-    	oceano = Cena(IMAGEM).vai()
-    	portao = Elemento(PORTAO_BRONZE, x=10, y=50, w=100, h=100, tit="Portao de Bronze",cena=oceano)
-    	palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, cena=oceano)
+         oceano = Cena(IMAGEM).vai()
+         portao = Elemento(PORTAO_BRONZE, x=10, y=50, w=100, h=100, tit="Portao de Bronze",cena=oceano)
+         palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, cena=oceano)
         
           
 IlhaProibida()
