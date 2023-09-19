29981fa008ca32392310712daf81df98dffa1601
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:15:37 -0300

    Saved grace/main.py @2023-09-19 15:15

--- grace/main.py
+++ grace/main.py
@@ -1,15 +1,15 @@
 # radia.grace.main.py
 # __author__ Finn
-from _spy.vitollino.main import Cena, Elemento
+from _spy.vitollino.main import Cena, Elemento, STYLE
 
-print("TEST")
+STYLE["width"] = 800
 
 IMAGEM = "https://i.imgur.com/my9MiVU.jpeg"
-PORTAO_BRONZE = "https://imgur.com/103Lydz"
+PORTAO_BRONZE = "https://i.imgur.com/103Lydz.png"
 
 class IlhaProibida:
     def __init__(self):
         oceano = Cena(IMAGEM).vai()
-        #portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
+        portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
         
 IlhaProibida()
