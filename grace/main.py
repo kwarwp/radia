f8863b6d663660a1e5ca57d3ae7d14fcb9367c41
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:58:23 -0300

    Created grace/main.py @2023-09-19 14:58

---
+++ grace/main.py
@@ -0,0 +1,12 @@
+# radia.grace.main.py
+# __author__ Finn
+from _spy.vitollino.main import Cena, Elemento
+IMAGEM = "https://imgur.com/a/cJCkxeA"
+PORATO_BRONZE = "https://imgur.com/103Lydz"
+
+class IlhaProibida:
+    def __init__(self):
+        oceano = Cena(IMAGEM).vai()
+        protao = Elemento(PORTAO_BRONZE), x=10, y=10, w=100, h=100, cena=oceano)
+        
+IlhaProibida()
