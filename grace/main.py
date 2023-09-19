b426289188de1b8058ccd4d01b7e850cfa24f15a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:58:52 -0300

    Saved grace/main.py @2023-09-19 14:58

--- grace/main.py
+++ grace/main.py
@@ -7,6 +7,6 @@ PORATO_BRONZE = "https://imgur.com/103Lydz"
 class IlhaProibida:
     def __init__(self):
         oceano = Cena(IMAGEM).vai()
-        protao = Elemento(PORTAO_BRONZE), x=10, y=10, w=100, h=100, cena=oceano)
+        protao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
         
 IlhaProibida()
