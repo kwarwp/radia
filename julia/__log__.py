e9f5555c53dd2017e24c032fee019e6ef081bf2a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:39:07 -0300

    Saved julia/__log__.py @2023-09-19 15:39

--- julia/__log__.py
+++ julia/__log__.py
@@ -78,3 +78,21 @@ NameError: name 'STYLE' is not defined
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Sep 19 2023 12:39:06.810 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    class Peao:
+  module <module> line 20
+    def move(self,ev=nome):
+NameError: name 'nome' is not defined
+'''},
