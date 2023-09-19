7334ff53b6a82231c5e66cf0425baa896ef1ffc0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:52:10 -0300

    Saved julia/__log__.py @2023-09-19 14:52

--- julia/__log__.py
+++ julia/__log__.py
@@ -13,3 +13,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Sep 19 2023 11:52:09.971 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  oceano = Cena(IMAGEM).vai()
+  ^
+IndentationError: expected an indented block
+'''},
