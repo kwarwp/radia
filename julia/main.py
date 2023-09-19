1e74004cf89104f59adeca2dd917ecc7c0b170cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:46:11 -0300

    Saved julia/main.py @2023-09-19 15:46

--- julia/main.py
+++ julia/main.py
@@ -8,17 +8,17 @@ PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
 PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
 PEAO = "https://imgur.com/zO3kiRp.png"
 class IlhaProibida:
-	def __init__(self):
-         oceano = Cena(IMAGEM).vai()
-         portao = Elemento(PORTAO_BRONZE, x=10, y=50, w=100, h=100, tit="Portao de Bronze",cena=oceano)
-         palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, cena=oceano)
-         self.peao = Peao(oceano)
+     def __init__(self):
+        oceano = Cena(IMAGEM).vai()
+        portao = Elemento(PORTAO_BRONZE, x=10, y=50, w=100, h=100, tit="Portao de Bronze",cena=oceano)
+        palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, cena=oceano)
+        self.peao = Peao(oceano)
 class Peao:
     def __init__(self, oceano):
          peao = Elemento(PEAO, x=30, y=60, w=60, h=80, cena=oceano, vai=self.move)
         
     def move(self,ev=None):
-            self.peao.x = 150
+        self.peao.x = 150
         
           
 IlhaProibida()
