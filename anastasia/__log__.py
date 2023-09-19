7b6808287f5574cc2ad0c2305575b85b51b7851f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:37:51 -0300

    Saved anastasia/__log__.py @2023-09-19 15:37

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -217,3 +217,22 @@ Traceback (most recent call last):
     self.peao = Peao (oceano)
 TypeError: __init__() takes 1 positional argument but more were given
 '''},
+{'date': 'Tue Sep 19 2023 11:54:50.944 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''alo
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    IlhaProibida()
+  module <module> line 19
+    self.peao = Peao (oceano)
+TypeError: __init__() takes 1 positional argument but more were given
+'''},
