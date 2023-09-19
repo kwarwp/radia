b62bf1a2c90f40518622a3b46311db43a4d867f8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:57:54 -0300

    Saved anastasia/main.py @2023-09-19 14:57

--- anastasia/main.py
+++ anastasia/main.py
@@ -4,11 +4,11 @@ print("alo")#escrito
 from _spy.vitollino.main import Cena, Elemento #Cena C maiusculo porque é classe  Elemento peçinhas que colocam na cena 
 IMAGEM = "https://www.mafiadomergulho.com.br/wp-content/uploads/2021/04/fundo-do-mar-site-copy.jpg" #COLOCAR IMAGEM DA INTERNET 
 PORTAO_BRONZE = "https://imgur.com/bl61b7H.jpg"
-
+PALACIO_CORAL = "https://imgur.com/bl61b7H.jpg"
 
 class IlhaProibida:  # : significa inicio de um bloco então posteriormente tem que estar intendado
     def __init__(self): #construcao, o primeiro parametro sempre se chama self
         oceano = Cena(IMAGEM).vai()
         portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
-        
+        palacio = Elemento(PALACIO_CORAL, X=20, Y=20, W=100, H=100, cena=oceano)
 IlhaProibida()
