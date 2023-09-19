64f99687dd689aab258f6c5ce66311240b9976ff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:14:37 -0300

    Saved sara/main.py @2023-09-19 15:14

--- sara/main.py
+++ sara/main.py
@@ -5,15 +5,15 @@ STYLE["width"] = 1000
 IMAGEM = 'https://dinamicambiental.com.br/wp-content/uploads/2021/06/o-oceano-e-a-nossa-sobrevivencia.jpg'
 PORTAO_BRONZE = 'https://thumbs.dreamstime.com/z/batedor-de-bronze-em-forma-uma-cabe%C3%A7a-le%C3%A3o-do-port%C3%A3o-da-catedral-col%C3%B4nia-na-alemanha-194660830.jpg?w=992'
 PALACIO_CORAL = 'https://img.freepik.com/fotos-premium/palacio-subaquatico-com-ilustracao-colorida-de-ia-generativa-de-corais-e-peixes_268722-1554.jpg?w=740'
-PEAO = 'https://w7.pngwing.com/pngs/380/600/png-transparent-spinning-tops-desktop-spinning-top-presentation-cartoon-area.png'
+PEAO = 'https://w1.pngwing.com/pngs/856/1022/png-transparent-beyblade-spinning-tops-drawing-toy-trompo-spin-top-fighter-beyblade-revolution-spinning-tops-battle.png'
 #Cena é classe e cena é atributo
 
 class IlhaProibida:
     def __init__(self):
         oceano = Cena(IMAGEM).vai()
         portao = Elemento(PORTAO_BRONZE, x=10, y=50, w=100, h=100, tit='Portão de Bronze', cena=oceano)
-        portao = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, tit='Palácio de Coral', cena=oceano)
-        portao = Elemento(PEAO, x=150, y=50, w=100, h=100, tit='Palácio de Coral', cena=oceano)
+        palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, tit='Palácio de Coral', cena=oceano)
+        peao = Elemento(PEAO, x=20, y=50, w=40, h=40, tit='Palácio de Coral', cena=oceano)
         
         
 
