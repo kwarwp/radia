c2beb97a4e22a731ccfbf11a726c75dac41e6beb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:51:36 -0300

    Saved roxanne/main.py @2023-09-19 14:51

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,8 +1,14 @@
 # radia.roxanne.main.py
 # __author__ Carlo
 from _spy.vitollino.main import Cena, Elemento
-IMAGEM = 'https://www.comciencia.br/wp-content/uploads/2021/09/oceano2-672x372.jpg'
-PORTAO_BRONZE= "https://img.freepik.com/fotos-premium/fragmento-de-portao-de-bronze-de-ferro-forjado_172420-936.jpg?w=2000"
-oceano = Cena(IMAGEM).vai()
-portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
+IMAGEM = "https://imgur.com/gVHmY2v.jpg"
+PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
 
+
+class IlhaProibida:
+    def __init__(self):
+        oceano = Cena(IMAGEM).vai()
+        portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
+
+
+IlhaProibida()
