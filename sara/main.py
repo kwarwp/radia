005ff9d776594fba78d82e34699ee6eef31252e1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:19:34 -0300

    Saved sara/main.py @2023-09-19 15:19

--- sara/main.py
+++ sara/main.py
@@ -5,7 +5,7 @@ STYLE["width"] = 1000
 IMAGEM = 'https://dinamicambiental.com.br/wp-content/uploads/2021/06/o-oceano-e-a-nossa-sobrevivencia.jpg'
 PORTAO_BRONZE = 'https://thumbs.dreamstime.com/z/batedor-de-bronze-em-forma-uma-cabe%C3%A7a-le%C3%A3o-do-port%C3%A3o-da-catedral-col%C3%B4nia-na-alemanha-194660830.jpg?w=992'
 PALACIO_CORAL = 'https://img.freepik.com/fotos-premium/palacio-subaquatico-com-ilustracao-colorida-de-ia-generativa-de-corais-e-peixes_268722-1554.jpg?w=740'
-PEAO = 'https://w1.pngwing.com/pngs/471/728/png-transparent-soap-bubble-toy-ball-spinning-tops-mundo-bita-drawing-nick-wilde-game-thumbnail.png'
+PEAO = 'https://www.designi.com.br/images/preview/10257242.jpg'
 #Cena é classe e cena é atributo
 
 class IlhaProibida:
