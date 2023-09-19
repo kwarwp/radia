277905136d914f9f9c69ebbfbb328ce5b357e8c8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:58:41 -0300

    Saved grace/__log__.py @2023-09-19 14:58

--- grace/__log__.py
+++ grace/__log__.py
@@ -6,3 +6,21 @@
                                                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 19 2023 11:58:41.23 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    IlhaProibida()
+  module <module> line 10
+    protao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
+NameError: name 'PORTAO_BRONZE' is not defined
+'''},
