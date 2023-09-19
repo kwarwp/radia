586cb8cdd172e4608991c3d98b8e9332cb90a179
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:00:25 -0300

    Saved grace/main.py @2023-09-19 15:00

--- grace/main.py
+++ grace/main.py
@@ -2,7 +2,7 @@
 # __author__ Finn
 from _spy.vitollino.main import Cena, Elemento
 IMAGEM = "https://imgur.com/a/cJCkxeA"
-PORATO_BRONZE = "https://imgur.com/103Lydz"
+PORTAO_BRONZE = "https://imgur.com/103Lydz"
 
 class IlhaProibida:
     def __init__(self):
