0827c76cf824ce3b2fdda80992c4b4f9d8815141
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:46:34 -0300

    Saved anastasia/main.py @2023-09-19 14:46

--- anastasia/main.py
+++ anastasia/main.py
@@ -2,7 +2,7 @@
 #_author_vanessa
 print("alo")#escrito
 from _spy.vitollino.main import Cena, Elemento #Cena C maiusculo porque é classe  Elemento peçinhas que colocam na cena 
-IMAGEM= 'https://www.mafiadomergulho.com.br/wp-content/uploads/2021/04/fundo-do-mar-site-copy.jpg'#COLOCAR IMAGEM DA INTERNET 
-PORTAO_BRONZE="https://imgur.com/gallery/jMXhMX6"
+IMAGEM = "https://www.mafiadomergulho.com.br/wp-content/uploads/2021/04/fundo-do-mar-site-copy.jpg" #COLOCAR IMAGEM DA INTERNET 
+PORTAO_BRONZE = "https://imgur.com/gallery/jMXhMX6"
 oceano = Cena(IMAGEM).vai()
 portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
