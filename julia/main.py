f2d9da1102829ac4ec52dd6c4a9520bd6b1e6ed2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:23:16 -0300

    Saved julia/main.py @2023-09-19 15:23

--- julia/main.py
+++ julia/main.py
@@ -1,6 +1,8 @@
 # radia.julia.main.pyhttp://supygirls.pythonanywhere.com/site/help.html
 # autor Bruno
-from _spy.vitollino.main import Cena, Elemento
+from _spy.vitollino.main import Cena, Elemento, STYLE
+STYLE["width"] = 1000
+STYLE["height"] = "600px"
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
 PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
 PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
@@ -10,7 +12,7 @@ class IlhaProibida:
          oceano = Cena(IMAGEM).vai()
          portao = Elemento(PORTAO_BRONZE, x=10, y=50, w=100, h=100, tit="Portao de Bronze",cena=oceano)
          palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, cena=oceano)
-         peao = Elemento(PEAO, x=30, y=60, w=40, h=80, cena=oceano)
+         peao = Elemento(PEAO, x=30, y=60, w=60, h=80, cena=oceano)
         
           
 IlhaProibida()
