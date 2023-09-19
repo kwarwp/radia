7fe60a9c48ccee2c933c0d322df9362d969485ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:32:03 -0300

    Saved morgan/main.py @2023-09-19 15:32

--- morgan/main.py
+++ morgan/main.py
@@ -2,9 +2,11 @@
 # __author__ Renan
 from _spy.vitollino.main import Cena, Elemento, STYLE
 STYLE["width"] = 800
+STYLE["height"] = "600px"
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
 PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
 PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
+PAWN = "https://imgur.com/zO3kiRp.png"
 
 
 class IlhaProibida:
@@ -13,6 +15,10 @@ class IlhaProibida:
         portao = Elemento(PORTAO_BRONZE, x=10, y=50,
         w=100, h=100, tit="Portão de Bronze", cena=oceano)
         palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, cena=oceano)
+        self.peao = Peao(oceano)
 
+class Peao:
+    def __init__(self, oceano):
+        peao = Elemento(PAWN, x=20, y=70, w=80, h= 80, cena=oceano)
 
 IlhaProibida()
