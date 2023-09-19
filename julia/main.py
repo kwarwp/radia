24f3be098edfdcd6d2d159d1518959fb227bb9b8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 11:51:30 -0300

    Saved julia/main.py @2023-09-19 14:51

--- julia/main.py
+++ julia/main.py
@@ -1,2 +1,12 @@
 # radia.julia.main.py
 # autor Bruno
+from _spy.vitollino.main import Cena, Elemento
+IMAGEM = "https://imgur.com/gVHmY2v.jpg"
+PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
+
+class IlhaProibida:
+	def __init__(self):
+    	oceano = Cena(IMAGEM).vai()
+          portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=1--, cena=oceano)
+          
+IlhaProibida()
