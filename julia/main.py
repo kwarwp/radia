5edd0f7e6f1c93d6a14faa3bc1d0bf5fc5f0df7e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:18:17 -0300

    Saved julia/main.py @2023-09-19 15:18

--- julia/main.py
+++ julia/main.py
@@ -4,13 +4,13 @@ from _spy.vitollino.main import Cena, Elemento
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
 PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
 PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
-PEAO = "https://imgur.com/z03KiRp.png"
+PEAO = "https://imgur.com/zO3kiRp.png"
 class IlhaProibida:
 	def __init__(self):
          oceano = Cena(IMAGEM).vai()
          portao = Elemento(PORTAO_BRONZE, x=10, y=50, w=100, h=100, tit="Portao de Bronze",cena=oceano)
          palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, cena=oceano)
-         peao = Elemento(PEAO, x=20, y=90, w=40, h=80, cena=oceano)
+         peao = Elemento(PEAO, x=30, y=60, w=40, h=80, cena=oceano)
         
           
 IlhaProibida()
