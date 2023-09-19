57c0f5412f5dba665c57c74626f7965067e0e540
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:37:22 -0300

    Saved anastasia/main.py @2023-09-19 15:37

--- anastasia/main.py
+++ anastasia/main.py
@@ -7,7 +7,7 @@ STYLE["height"] = "600px"
 IMAGEM = "https://imgur.com/gVHmY2v.jpg" #COLOCAR IMAGEM DA INTERNET 
 PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
 PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
-PEAO = "https://imgur.com/zO3kiRp.jpg"
+PEAO = "https://imgur.com/zO3kiRp.png"
 
 
 
@@ -15,7 +15,7 @@ class IlhaProibida:  # : significa inicio de um bloco então posteriormente tem 
     def __init__(self): #construcao, o primeiro parametro sempre se chama self
         oceano = Cena(IMAGEM).vai()
         portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, tit="Portão de Bronze", cena=oceano)
-        palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, tit="Palacio coral", cena=oceano)
+        palacio = Elemento(PALACIO_CORAL, x=120, y=10, w=100, h=100, tit="Palacio coral", cena=oceano)
         self.peao = Peao (oceano)
 
 class Peao:  
