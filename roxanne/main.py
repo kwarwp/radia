0ffd13c76a39e3e76fa2ac0758fe3672070f266c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:10:23 -0300

    Saved roxanne/main.py @2023-09-19 15:10

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,14 +1,18 @@
 # radia.roxanne.main.py
 # __author__ Carlo
-from _spy.vitollino.main import Cena, Elemento
+from _spy.vitollino.main import Cena, Elemento, STYLE
+STYLE["width"] = 800
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
 PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
+PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
 
 
 class IlhaProibida:
     def __init__(self):
         oceano = Cena(IMAGEM).vai()
-        portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, cena=oceano)
+        portao = Elemento(PORTAO_BRONZE, x=10, y=50,
+        w=100, h=100, tit="Portão de Bronze", cena=oceano)
+        palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, cena=oceano)
 
 
 IlhaProibida()
