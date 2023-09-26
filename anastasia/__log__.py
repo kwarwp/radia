d42bfeb50227fae1dd84096b4336c8429cf95147
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:06:10 -0300

    Saved anastasia/__log__.py @2023-09-26 15:06

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -319,3 +319,22 @@ NameError: name 'local' is not defined
                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 26 2023 12:06:09.92 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''alo
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 35
+    IlhaProibida()
+  module <module> line 15
+    self.Terreno1 = Terreno(PORTAO_BRONZE, posx=10, posy=50, cena=oceano)
+TypeError: object() takes no parameters
+'''},
