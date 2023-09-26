d118857a1ff04703a5cdaa2e6cb6bfaa882fd6a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:08:24 -0300

    Saved grace/__log__.py @2023-09-26 15:08

--- grace/__log__.py
+++ grace/__log__.py
@@ -122,3 +122,21 @@ TypeError: 'Terreno' object is not iterable
     self.terrenos.push(Terreno(local, position, 10))
 AttributeError: 'list' object has no attribute 'push'
 '''},
+{'date': 'Tue Sep 26 2023 12:08:23.493 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    IlhaProibida()
+  module <module> line 23
+    self.terrenos.push(Terreno(local, position, 10))
+AttributeError: 'list' object has no attribute 'push'
+'''},
