38649927d654bb13a4c7a1ded8b658e4f70c1913
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:42:39 -0300

    Saved grace/__log__.py @2023-09-26 15:42

--- grace/__log__.py
+++ grace/__log__.py
@@ -185,3 +185,23 @@ SyntaxError: invalid syntax
     peao.mover(self.posx, self.posy)
 TypeError: mover() takes 2 positional arguments but more were given
 '''},
+{'date': 'Tue Sep 26 2023 12:42:38.476 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 52
+    IlhaProibida()
+  module <module> line 27
+    self.terrenos[2].ocupa(self.pawn)
+  module <module> line 39
+    peao.mover(self.posx, self.posy)
+TypeError: mover() takes 2 positional arguments but more were given
+'''},
