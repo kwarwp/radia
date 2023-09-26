bd7c50cee39b85b3ebb08d490fe489a454ed11df
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:00:15 -0300

    Saved julia/main.py @2023-09-26 15:00

--- julia/main.py
+++ julia/main.py
@@ -1,29 +1,31 @@
 # radia.julia.main.pyhttp://supygirls.pythonanywhere.com/site/help.html
 # autor Bruno
 from _spy.vitollino.main import Cena, Elemento, STYLE
-STYLE["width"] = 1000
+STYLE["width"] = 800
 STYLE["height"] = "600px"
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
 PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
 PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
-PEAO = "https://imgur.com/zO3kiRp.png"
+PAWN = "https://imgur.com/zO3kiRp.png"
+
+
 class IlhaProibida:
-     def __init__(self):
+    def __init__(self):
         oceano = Cena(IMAGEM).vai()
-        self.terreno1 = Terreno(PORTAO_BRONZE, posx=10, posy=50, cena=oceano)
-        self.terreno2 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
+        self.terreno = Terreno(PORTAO_BRONZE, posx=10, posy=50,
+        cena=oceano)
+        self.terreno1 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
         self.peao = Peao(oceano)
-        
+
 class Terreno:
-    def __init__(self, local, posx, posy, oceano):
-        self.local = Elemento(local, x=posx, y=posy, w=100, h=100, cena=oceano)
-        
+    def __init__(self, local, posx, posy, cena):
+        self.local = Elemento(local, x=posx, y=posy, w=100, h= 100,
+        cena=cena)
+
 class Peao:
     def __init__(self, oceano):
-         self.peao = Elemento(PEAO, x=30, y=60, w=60, h=80, cena=oceano, vai=self.move)
+        self.peao = Elemento(PAWN, x=20, y=70, w=80, h= 80,
+        cena=oceano, vai=self.move)
         
-    def move(self,ev=None):
-        self.peao.x = 150
-        
-          
-IlhaProibida()
+    def move(self, ev=None):
+        self.peao.x = 170
