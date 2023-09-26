61ac87693fee282a1c9ae972d71e728fb3ad631f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:06:49 -0300

    Saved grace/__log__.py @2023-09-26 15:06

--- grace/__log__.py
+++ grace/__log__.py
@@ -67,3 +67,21 @@ SyntaxError: invalid syntax
     pawn = Elemento(PAWN, x=10, y=10, w=100, h=100, cena=oceano)
 NameError: name 'oceano' is not defined
 '''},
+{'date': 'Tue Sep 26 2023 12:06:49.239 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    IlhaProibida()
+  module <module> line 23
+    self.terrenos.push(Terreno(loca, position, 10))
+AttributeError: 'list' object has no attribute 'push'
+'''},
