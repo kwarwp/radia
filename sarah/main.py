a32a5d201b180a225a73e688d38e4130007dd9a7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 11:41:48 -0300

    Created sarah/main.py @2023-09-26 14:41

---
+++ sarah/main.py
@@ -0,0 +1,34 @@
+# radia.sarah.main.py
+# __author__ Filipe
+from _spy.vitollino.main import Cena, Elemento, STYLE
+STYLE["width"] = 800
+STYLE["height"] = "600px"
+IMAGEM = "https://imgur.com/gVHmY2v.jpg"
+PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
+PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
+PAWN = "https://imgur.com/zO3kiRp.png"
+
+
+class IlhaProibida:
+    def __init__(self):
+        oceano = Cena(IMAGEM).vai()
+        portao = Elemento(PORTAO_BRONZE, x=10, y=50,
+        w=100, h=100, tit="Portão de Bronze", cena=oceano)
+        palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, cena=oceano)
+        self.peao = Peao(oceano)
+
+class Terreno:
+    def __init__(self, local, posx, posy, oceano):
+        self.local = Elemento(local, x=posx, y=posy, w=100, h= 100,
+        cena=oceano)
+
+class Peao:
+    def __init__(self, oceano):
+        self.peao = Elemento(PAWN, x=20, y=70, w=80, h= 80,
+        cena=oceano, vai=self.move)
+        
+    def move(self, ev=None):
+        self.peao.x = 170
+        
+
+IlhaProibida()
