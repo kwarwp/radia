b6b7e4b71463d00ad0f37cd9509dd690673bfdb5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:08:21 -0300

    Saved anastasia/__log__.py @2023-09-26 15:08

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -338,3 +338,22 @@ Traceback (most recent call last):
     self.Terreno1 = Terreno(PORTAO_BRONZE, posx=10, posy=50, cena=oceano)
 TypeError: object() takes no parameters
 '''},
+{'date': 'Tue Sep 26 2023 12:08:20.359 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''alo
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    IlhaProibida()
+  module <module> line 18
+    self.terrenos = [Terrenos(cena=oceano, posy=50, posx=px, local=lc) for px, lc in info_terrenos]
+NameError: name 'Terrenos' is not defined
+'''},
