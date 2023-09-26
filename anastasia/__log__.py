e88fc24ac640e96559588f614e1e72c62dff5c8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:11:49 -0300

    Saved anastasia/__log__.py @2023-09-26 15:11

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -357,3 +357,22 @@ Traceback (most recent call last):
     self.terrenos = [Terrenos(cena=oceano, posy=50, posx=px, local=lc) for px, lc in info_terrenos]
 NameError: name 'Terrenos' is not defined
 '''},
+{'date': 'Tue Sep 26 2023 12:11:49.151 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''alo
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    IlhaProibida()
+  module <module> line 18
+    self.terrenos = [Terrenos(cena=oceano, posy=50, posx=px, local=lc) for px, lc in info_terrenos]
+NameError: name 'Terrenos' is not defined
+'''},
