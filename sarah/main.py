7c6c2181e7270cca0c49406783ec0efede3e1a01
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:47:14 -0300

    Saved sarah/main.py @2023-09-26 15:47

--- sarah/main.py
+++ sarah/main.py
@@ -1,5 +1,5 @@
 # radia.sarah.main.py
-# __author__ Filipe
+# __author__ Filipe Marçal
 from _spy.vitollino.main import Cena, Elemento, STYLE
 STYLE["width"] = 800
 STYLE["height"] = "600px"
@@ -12,27 +12,37 @@ PAWN = "https://imgur.com/zO3kiRp.png"
 class IlhaProibida:
     def __init__(self):
         oceano = Cena(IMAGEM).vai()
-        self.terreno = Terreno(PORTAO_BRONZE, posx=10, posy=50,
-        cena=oceano)
-        self.terreno1 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
-        info_terrenos= [(10, PORTAO_BRONZE), (120, PALACIO_CORAL), (230, PORTAO_BRONZE)]
+        #self.terreno = Terreno(PORTAO_BRONZE, posx=10, posy=50,
+        #cena=oceano)
+        #self.terreno1 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
+        #info_terrenos= [(10, PORTAO_BRONZE), (120, PALACIO_CORAL), (230, PORTAO_BRONZE)]
         info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL]
         self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
         for px, lc in enumerate(info_terrenos)]
         self.peao = Peao(oceano)
+        self.terrenos[2].ocupa(self.peao)
 
 class Terreno:
     def __init__(self, local, posx, posy, cena):
         self.local = Elemento(local, x=posx, y=posy, w=100, h= 100,
         cena=cena)
+        self.peao = None
+        self.posx, self.posy = posx, posy
+        
+    def ocupa(self, peao):
+        self.peao = peao
+        peao.mover(self.posx)
 
 class Peao:
     def __init__(self, oceano):
         self.peao = Elemento(PAWN, x=20, y=70, w=80, h= 80,
         cena=oceano, vai=self.move)
         
-    def move(self, ev=None):
-        self.peao.x = 150
+    def move(self, ev=None):  # Corrigir: não está condizente!
+        self.peao.x = 170
+        
+    def mover(self, x):
+        self.peao.x = x
         
 
 IlhaProibida()
