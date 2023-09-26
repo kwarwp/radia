9ef104878a74769d5f8f356b7df4c54e1db05fe2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:43:52 -0300

    Saved sara/__log__.py @2023-09-26 15:43

--- sara/__log__.py
+++ sara/__log__.py
@@ -365,3 +365,21 @@ NameError: name 'peao' is not defined
     self.terrenos[2].ocupa(self,peao)
 NameError: name 'peao' is not defined
 '''},
+{'date': 'Tue Sep 26 2023 12:43:54.366 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    IlhaProibida()
+  module <module> line 24
+    self.terrenos[2].ocupa(self,peao)
+NameError: name 'peao' is not defined
+'''},
