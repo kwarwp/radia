68036c9778513b32ee7e1b8b52ce9c60f84b4b83
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:54:43 -0300

    Saved sarah/main.py @2023-09-26 15:54

--- sarah/main.py
+++ sarah/main.py
@@ -12,10 +12,10 @@ PAWN = "https://imgur.com/zO3kiRp.png"
 class IlhaProibida:
     def __init__(self):
         oceano = Cena(IMAGEM).vai()
-        self.terreno = Terreno(PORTAO_BRONZE, posx=10, posy=50,
-        cena=oceano)
-        self.terreno1 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
-        info_terrenos= [(10, PORTAO_BRONZE), (120, PALACIO_CORAL), (230, PORTAO_BRONZE)]
+        #self.terreno = Terreno(PORTAO_BRONZE, posx=10, posy=50,
+        #cena=oceano)
+        #self.terreno1 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
+        #info_terrenos= [(10, PORTAO_BRONZE), (120, PALACIO_CORAL), (230, PORTAO_BRONZE)]
         info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL]
         self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
         for px, lc in enumerate(info_terrenos)]
@@ -31,18 +31,21 @@ class Terreno:
         
     def ocupa(self, peao):
         self.peao = peao
-        peao.mover(self.posx)
+        peao.mover(self.posx, self)
 
 class Peao:
     def __init__(self, oceano):
         self.peao = Elemento(PAWN, x=20, y=70, w=80, h= 80,
         cena=oceano, vai=self.move)
+        self.terreno = None
         
     def move(self, ev=None):  # Corrigir: não está condizente!
         self.peao.x = 170
         
-    def mover(self, x):
+    def mover(self, x, terreno):
+        self.terreno = terreno
         self.peao.x = x
+
         
 
 IlhaProibida()
