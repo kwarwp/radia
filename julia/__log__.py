770520be7e48cc664bd6bea534e95481d6c5cc4e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 11:53:23 -0300

    Saved julia/__log__.py @2023-09-26 14:53

--- julia/__log__.py
+++ julia/__log__.py
@@ -204,3 +204,21 @@ TypeError: __init__() got an unexpected keyword argument 'cena'
     self.terreno1 = Terreno(PORTAO_BRONZE, 10, 50, "Portao de Bronze",cena=oceano)
 TypeError: __init__() got an unexpected keyword argument 'cena'
 '''},
+{'date': 'Tue Sep 26 2023 11:53:23.144 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 29
+    IlhaProibida()
+  module <module> line 13
+    self.terreno1 = Terreno(PORTAO_BRONZE, posx=10, posy=50, titulo="Portao de Bronze", cena=oceano)
+TypeError: __init__() got an unexpected keyword argument 'cena'
+'''},
