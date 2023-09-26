c50a0b1bb8cd81809ff5734981bea5d8abc87abd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:47:40 -0300

    Saved anastasia/__log__.py @2023-09-26 15:47

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -486,3 +486,24 @@ Traceback (most recent call last):
     self.peao = Elemento(PAWN, x=20, y=70, w=80, h=80,
 AttributeError: 'Peao' object has no attribute 'move'
 '''},
+{'date': 'Tue Sep 26 2023 12:47:38.447 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''alo
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 49
+    IlhaProibida()
+  module <module> line 22
+    self.terrenos[2].ocupa(self.peao) #chamou o terreno 1 e ocupa com o peão 
+  module <module> line 34
+    peao.mover(self.posx, terreno)
+NameError: name 'terreno' is not defined
+'''},
