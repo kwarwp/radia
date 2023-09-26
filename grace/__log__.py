b0e7bdf152ed868ff0e484d490f6f7b39729f5cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 12:14:04 -0300

    Saved grace/__log__.py @2023-09-26 15:14

--- grace/__log__.py
+++ grace/__log__.py
@@ -140,3 +140,21 @@ AttributeError: 'list' object has no attribute 'push'
     self.terrenos.push(Terreno(local, position, 10))
 AttributeError: 'list' object has no attribute 'push'
 '''},
+{'date': 'Tue Sep 26 2023 12:14:03.778 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    IlhaProibida()
+  module <module> line 20
+    self.terrenos = [Terreno(l, p, i * 100 + 10, oceano) for i, l, p in enumerate(info_terrenos)]
+ValueError: need more than 2 values to unpack
+'''},
