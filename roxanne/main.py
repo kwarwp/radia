a79300a45b195e9ce3af81edaf738d05ae96a166
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 11:51:43 -0300

    Saved roxanne/main.py @2023-09-26 14:51

--- roxanne/main.py
+++ roxanne/main.py
@@ -12,15 +12,15 @@ PAWN = "https://imgur.com/zO3kiRp.png"
 class IlhaProibida:
     def __init__(self):
         oceano = Cena(IMAGEM).vai()
-        self.terreno = Terreno(PORTAO_BRONZE, x=10, y=50,
+        self.terreno = Terreno(PORTAO_BRONZE, posx=10, posy=50,
         cena=oceano)
         palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, cena=oceano)
         self.peao = Peao(oceano)
 
 class Terreno:
-    def __init__(self, local, posx, posy, oceano):
+    def __init__(self, local, posx, posy, cena):
         self.local = Elemento(local, x=posx, y=posy, w=100, h= 100,
-        cena=oceano)
+        cena=cena)
 
 class Peao:
     def __init__(self, oceano):
