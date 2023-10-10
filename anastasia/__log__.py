2aacd8759c15252f84b104abe78b51e24a858b6c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 12:41:38 -0300

    Saved anastasia/__log__.py @2023-10-10 15:41

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -843,3 +843,25 @@ NameError: name 'oceano' is not defined
     self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc) 
 NameError: name 'oceano' is not defined
 '''},
+{'date': 'Tue Oct 10 2023 12:41:31.884 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 111
+    IlhaProibida()
+  module <module> line 52
+    self.terrenos[1].ocupa(self.peao) #chamou o terreno 1 e ocupa com o peão
+  module <module> line 71
+    peao.mover(self.posx, self)
+  module <module> line 101
+    self.peao.y = y
+NameError: name 'y' is not defined
+'''},
