4410b5192f5339d9d962ad0c4cc1010407b339b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 20:41:15 -0300

    Saved kristen/__log__.py @2023-10-10 23:41

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -35,3 +35,23 @@ AttributeError: 'StringIO' object has no attribute '__exit__'
     TT().start_toml_editor(str_io="ola a tod")
 TypeError: start_toml_editor() got an unexpected keyword argument 'str_io'
 '''},
+{'date': 'Tue Oct 10 2023 20:41:14.776 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    TT().start_toml_editor(toml_="ola a tod")
+  module <module> line 26
+    self.load_(str_io=StringIO(toml_))
+  module <module> line 5
+    with file_ as avt:
+AttributeError: 'StringIO' object has no attribute '__exit__'
+'''},
