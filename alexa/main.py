34597e601ee3662d5a9e5bb21933a06cdb020703
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 09:55:01 -0300

    Created alexa/main.py @2023-10-10 12:55

---
+++ alexa/main.py
@@ -0,0 +1,15 @@
+# radia.alexa.main.py
+# AA - Anderson Amorim (ouvinte)
+
+from _spy.vitollino.main import Cena, Elemento, Style
+
+
+
+IMAGEM = 'https://dinamicambiental.com.br/wp-content/uploads/2021/06/o-oceano-e-a-nossa-sobrevivencia.jpg'
+PORTAO_BRONZE = 'https://thumbs.dreamstime.com/z/batedor-de-bronze-em-forma-uma-cabe%C3%A7a-le%C3%A3o-do-port%C3%A3o-da-catedral-col%C3%B4nia-na-alemanha-194660830.jpg?w=992'
+PALACIO_CORAL = 'https://img.freepik.com/fotos-premium/palacio-subaquatico-com-ilustracao-colorida-de-ia-generativa-de-corais-e-peixes_268722-1554.jpg?w=740'
+VALE_TENEBROSO = 'http://2.bp.blogspot.com/-lo6Hr8UD4D4/T8FDTidezEI/AAAAAAAAAD0/4zitS77Hoxg/s1600/vale+da+sombra+da+morte.bmp'
+PISTA = 'https://i0.wp.com/soepinaobasta.com/wp-content/uploads/2019/05/Heliponto.jpg?resize=1000%2C500&ssl=1'
+PEAO = 'https://imgur.com/zO3kiRp.png'
+
+STYLE["width"] = 1000
