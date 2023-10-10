fea7f751825982e8880f6fafa5051b51ada6cb39
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 20:48:56 -0300

    Saved sarah/main.py @2023-10-10 23:48

--- sarah/main.py
+++ sarah/main.py
@@ -6,16 +6,22 @@ STYLE["height"] = "600px"
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
 PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
 PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
+HELICOPTERO = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/LAPD_Bell_206_Jetranger.jpg/450px-LAPD_Bell_206_Jetranger.jpg"
+PANTANO = "https://cdn.pixabay.com/photo/2015/07/15/01/09/sunset-845552_1280.jpg"
 PAWN = "https://imgur.com/zO3kiRp.png"
+BOSQUE = "https://cdn.pixabay.com/photo/2018/04/06/00/25/trees-3294681_1280.jpg"
 
 
 class IlhaProibida:
     def __init__(self):
         oceano = Cena(IMAGEM).vai()
-        #self.terreno = Terreno(PORTAO_BRONZE, posx=10, posy=50,
-        #cena=oceano)
-        #self.terreno1 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
-        #info_terrenos= [(10, PORTAO_BRONZE), (120, PALACIO_CORAL), (230, PORTAO_BRONZE)]
+        self.terreno = Terreno(PORTAO_BRONZE, posx=10, posy=50, cena=oceano)
+        self.terreno1 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
+        self.terreno2= Terreno(HELICOPTERO, posx=10, posy=170, cena=oceano)
+        self.terreno3 = Terreno(PALACIO_CORAL, posx=120, posy=170, cena=oceano)
+        self.terreno4 = Terreno(PANTANO, posx=230, posy= 170, cena=oceano)
+        self.terreno5 = Terreno(BOSQUE, posx=340, posy=170, cena=oceano)
+        info_terrenos= [(10, PORTAO_BRONZE), (120, PALACIO_CORAL), (230, PORTAO_BRONZE)]
         info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL]
         self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
         for px, lc in enumerate(info_terrenos)]
