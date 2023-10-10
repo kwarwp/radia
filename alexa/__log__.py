0d06007bd156e4cce682b68e023de781703d5446
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 10:06:30 -0300

    Saved alexa/__log__.py @2023-10-10 13:06

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -123,3 +123,19 @@ Traceback (most recent call last):
   module <module> line 3
     from _spy.vitollino.main import Cena, Elemento, Style
 '''},
+{'date': 'Tue Oct 10 2023 10:06:29.136 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    IlhaProibida()
+NameError: name 'IlhaProibida' is not defined
+'''},
