9fc143ba94f3b47dd929a360f46562d0d61438f8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 11:38:22 -0300

    Saved julia/__log__.py @2023-10-10 14:38

--- julia/__log__.py
+++ julia/__log__.py
@@ -261,3 +261,10 @@ IndentationError: unexpected indent
                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 10 2023 11:38:22.324 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 48
+  """ Move o peão para a direita
+  ^
+IndentationError: expected an indented block
+'''},
