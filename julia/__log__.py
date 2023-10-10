cf763353cd9d8f46a02269b06705615199f5d6ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 13:17:18 -0300

    Saved julia/__log__.py @2023-10-10 16:17

--- julia/__log__.py
+++ julia/__log__.py
@@ -388,3 +388,22 @@ Traceback (most recent call last):
     if x==7:
 NameError: name 'x' is not defined
 '''},
+{'date': 'Tue Oct 10 2023 13:17:17.821 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Executado no modulo julia
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 97
+    IlhaProibida()
+  module <module> line 51
+    if x==7:
+NameError: name 'x' is not defined
+'''},
