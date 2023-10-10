19cfb4aa5ebb18692669c09e3253cd451ebbf618
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 12:11:03 -0300

    Saved anastasia/__log__.py @2023-10-10 15:11

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -732,3 +732,24 @@ Traceback (most recent call last):
   module <module> line 27
     from julia.main import IhaProibida as Ilha #importar o pacote julia módulo main, importar apenas a classe Ilha proibida e chamar de Ilha
 '''},
+{'date': 'Tue Oct 10 2023 12:10:56.888 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+    Marcador usado para definir a posição do jogador nos terrenos.
+    
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 107
+    IlhaProibida()
+  module <module> line 48
+    self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc) 
+NameError: name 'oceano' is not defined
+'''},
