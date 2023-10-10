c9181bd971eeeedae80553326bf9fdac7c08a14a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 20:37:03 -0300

    Created sarah/__log__.py @2023-10-10 23:37

---
+++ sarah/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Oct 10 2023 20:37:01.306 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
+                                                                                    ^
+SyntaxError: invalid syntax
+'''},
