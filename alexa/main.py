70d20fa0d893de25d1143e6491b2fd18157f40b9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 10:08:29 -0300

    Saved alexa/main.py @2023-10-10 13:08

--- alexa/main.py
+++ alexa/main.py
@@ -12,4 +12,47 @@ PEAO = 'https://imgur.com/zO3kiRp.png'
 STYLE["height"] = "600px"
 STYLE["width"] = 1000
 
+class IlhaProibida:
+    def __init__(self):
+        oceano = Cena(IMAGEM).vai()
+        #portao = Elemento(PORTAO_BRONZE, x=10, y=50, w=100, h=100, tit='Portão de Bronze', cena=oceano)
+        #palacio = Elemento(PALACIO_CORAL, x=120, y=50, w=100, h=100, tit='Palácio de Coral', cena=oceano)
+        #peao = Elemento(PEAO, x=20, y=70, w=80, h=80, tit='Palácio de Coral', cena=oceano)
+        info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, VALE_TENEBROSO, PISTA]
+        self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
+        for px, lc in enumerate(info_terrenos)]
+        self.peao = Peao(oceano)
+        self.terrenos[2].ocupa(self.peao)
+        
+    def direita(self, terreno):
+        aqui = self.terrenos.index(terreno)
+        return.self.terreno[aqui+1]
+        
+class Terreno:
+    def __init__(self, local, posx, posy, cena):
+        self.local = Elemento(local, x=posx, y=posy, w=100, h=100, cena=cena)
+        self.peao = None
+        self.posx, self.posy = posx, posy
+    
+    def ocupa(self, peao):
+        self.peao = peao
+        peao.mover(self.posx, self)
+
+class Peao:
+    def __init__(self, oceano):
+        self.peao = Elemento(PEAO, x=20, y=70, w=80, h= 80, cena=oceano, vai=self.move)
+        self.terreno = None
+        self.ilha = ilha
+        
+    def move(self, ev=None):
+        #self.peao.x = 130
+        terreno_destino = self.ilha.direita(self.terreno)
+        terreno_destino.ocupa(self)
+        
+    def mover(self, x, terreno):
+        self.terreno = terreno
+        self.peao.x = x
+
+
+
 IlhaProibida()
