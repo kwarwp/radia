5e7315dad98565b57603dafbd74c936beb1e7188
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 13:09:17 -0300

    Saved soraya/main.py @2023-10-10 16:09

--- soraya/main.py
+++ soraya/main.py
@@ -13,7 +13,7 @@ Changelog
     Versão Inicial (26).
 
 |   **Open Source Notification:** This file is part of open source program **Ilha Proibida**
-|   **Copyright © 2023  Carlo Oliveira** <carlo@nce.ufrj.br>,
+|   **Copyright © 2023  Keila Oliveira** <keila90.if@gmail.com>,
 |   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
 |   `Labase <http://labase.selfip.org/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
 """
