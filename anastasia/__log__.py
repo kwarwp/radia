6a91b01b4a891b8ae8b391715240e839e161aecc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 20:53:34 -0300

    Saved anastasia/__log__.py @2023-10-10 23:53

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -1125,3 +1125,21 @@ SyntaxError: invalid syntax
     info_terrenos = np.array([
 NameError: name 'np' is not defined
 '''},
+{'date': 'Tue Oct 10 2023 20:53:33.881 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 119
+    IlhaProibida()
+  module <module> line 51
+    info_terrenos = np.array([[0,1,0,1],[0,1,0,1]])
+AttributeError: 'module' object has no attribute 'array'
+'''},
