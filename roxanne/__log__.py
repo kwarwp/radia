fd92f9970c19f469aa686e2d7fd30163e06a5eae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 11:39:56 -0300

    Saved roxanne/__log__.py @2023-10-10 14:39

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -45,3 +45,19 @@ SyntaxError: invalid syntax
                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 10 2023 11:39:58.343 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 86
+    print(help(list))
+IOError: File https://cdn.jsdelivr.net/npm/brython@3.4.0/builtins_docstrings.js not found
+'''},
