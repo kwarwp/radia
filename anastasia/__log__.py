5beb87a2c903c3cd0b02dc19561d4078448b55cf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 21:36:58 -0300

    Saved anastasia/__log__.py @2023-10-11 00:36

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -1238,3 +1238,27 @@ IndentationError: expected an indented block
         ^
 SyntaxError: missing parenthesis in call to 'print'
 '''},
+{'date': 'Tue Oct 10 2023 21:36:57.815 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''0
+1
+2
+3
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 136
+    IlhaProibida()
+  module <module> line 66
+    self.peao = Peao(self)
+  module <module> line 96
+    self.peao = Elemento(PAWN, x=20, y=70, w=80, h=80,
+AttributeError: 'Peao' object has no attribute 'move'
+'''},
