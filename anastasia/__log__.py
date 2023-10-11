52b701dfc431508b809e167e52d064d8028966c9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 22:43:56 -0300

    Saved anastasia/__log__.py @2023-10-11 01:43

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -1708,3 +1708,21 @@ SyntaxError: invalid syntax
     info_terrenos = random.shuffle(lista_terrenos, resultado)
 NameError: name 'resultado' is not defined
 '''},
+{'date': 'Tue Oct 10 2023 22:43:55.301 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 131
+    IlhaProibida()
+  module <module> line 59
+    self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
+NameError: free variable 'info_terrenos' referenced before assignment in enclosing scope
+'''},
