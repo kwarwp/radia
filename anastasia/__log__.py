8a87d53d8641f746dbead0ab05e25c41af484ae3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 21:42:00 -0300

    Saved anastasia/__log__.py @2023-10-11 00:41

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -1298,3 +1298,21 @@ NameError: name 'py' is not defined
     self.terrenos = [Terreno(cena=oceano, posy=py*50, posx=px*110+20, local=lc) 
 NameError: name 'py' is not defined
 '''},
+{'date': 'Tue Oct 10 2023 21:41:59.383 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 129
+    IlhaProibida()
+  module <module> line 57
+    self.terrenos = [Terreno(cena=oceano, posy=py*50, posx=px*110+20, local=lc) 
+ValueError: need more than 2 values to unpack
+'''},
