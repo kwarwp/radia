4a40c9a8804fe1e0f57953007ad17de698b94ee6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 22:47:38 -0300

    Saved anastasia/__log__.py @2023-10-11 01:47

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -1726,3 +1726,21 @@ NameError: name 'resultado' is not defined
     self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
 NameError: free variable 'info_terrenos' referenced before assignment in enclosing scope
 '''},
+{'date': 'Tue Oct 10 2023 22:47:37.500 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 131
+    IlhaProibida()
+  module <module> line 59
+    self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
+NameError: free variable 'info_terrenos' referenced before assignment in enclosing scope
+'''},
