82bdd07e428a65bf9aa9a02263b6a4e36209d4dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 21:53:03 -0300

    Saved anastasia/__log__.py @2023-10-11 00:53

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -1384,3 +1384,23 @@ SyntaxError: invalid syntax
     self.terrenos = [Terreno(cena=oceano, posx=px*110+20, local=lc) 
 TypeError: __init__ missing 1 positional argument: 'posy'
 '''},
+{'date': 'Tue Oct 10 2023 21:53:02.955 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 125
+    IlhaProibida()
+  module <module> line 55
+    self.peao = Peao(self)
+  module <module> line 85
+    self.peao = Elemento(PAWN, x=20, y=70, w=80, h=80,
+AttributeError: 'Peao' object has no attribute 'move'
+'''},
