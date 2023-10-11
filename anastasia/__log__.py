2282d42eb50b55ed6c1e56a86dcbc5c44b38d5b9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 23:03:18 -0300

    Saved anastasia/__log__.py @2023-10-11 02:03

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -1762,3 +1762,21 @@ NameError: free variable 'info_terrenos' referenced before assignment in enclosi
     info_terrenos += random.shuffle(lista_terrenos)
 UnboundLocalError: local variable 'info_terrenos' referenced before assignment
 '''},
+{'date': 'Tue Oct 10 2023 23:03:17.944 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 142
+    IlhaProibida()
+  module <module> line 69
+    shuffle(info_terrenos)
+NameError: name 'shuffle' is not defined
+'''},
