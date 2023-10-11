8dd41da5de49327f43e863e6262526c3a715a9ec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-11 18:01:27 -0300

    Saved alexa/main.py @2023-10-11 21:01

--- alexa/main.py
+++ alexa/main.py
@@ -2,12 +2,12 @@
 # AA - Anderson Amorim (ouvinte)
 from _spy.vitollino.main import Cena, Elemento, STYLE
 
-IMAGEM = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fsegredosdomundo.r7.com%2Ffundo-do-mar-real%2F&psig=AOvVaw39wrJX08r73R0ivNIqbGqe&ust=1697032047707000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLDK_ZTP64EDFQAAAAAdAAAAABAFQ'
-PORTAO_BRONZE = 'https://thumbs.dreamstime.com/z/batedor-de-bronze-em-forma-uma-cabe%C3%A7a-le%C3%A3o-do-port%C3%A3o-da-catedral-col%C3%B4nia-na-alemanha-194660830.jpg?w=992'
-PALACIO_CORAL = 'https://img.freepik.com/fotos-premium/palacio-subaquatico-com-ilustracao-colorida-de-ia-generativa-de-corais-e-peixes_268722-1554.jpg?w=740'
-VALE_TENEBROSO = 'http://2.bp.blogspot.com/-lo6Hr8UD4D4/T8FDTidezEI/AAAAAAAAAD0/4zitS77Hoxg/s1600/vale+da+sombra+da+morte.bmp'
-PISTA = 'https://i0.wp.com/soepinaobasta.com/wp-content/uploads/2019/05/Heliponto.jpg?resize=1000%2C500&ssl=1'
-PEAO = 'https://imgur.com/zO3kiRp.png'
+IMAGEM = "https://i.imgur.com/my9MiVU.jpeg"
+PORTAO_BRONZE = "https://thumbs.dreamstime.com/z/batedor-de-bronze-em-forma-uma-cabe%C3%A7a-le%C3%A3o-do-port%C3%A3o-da-catedral-col%C3%B4nia-na-alemanha-194660830.jpg?w=992"
+PALACIO_CORAL = "https://img.freepik.com/fotos-premium/palacio-subaquatico-com-ilustracao-colorida-de-ia-generativa-de-corais-e-peixes_268722-1554.jpg?w=740"
+VALE_TENEBROSO = "http://2.bp.blogspot.com/-lo6Hr8UD4D4/T8FDTidezEI/AAAAAAAAAD0/4zitS77Hoxg/s1600/vale+da+sombra+da+morte.bmp"
+PISTA = "https://i0.wp.com/soepinaobasta.com/wp-content/uploads/2019/05/Heliponto.jpg?resize=1000%2C500&ssl=1"
+PEAO = "https://imgur.com/zO3kiRp.png"
 
 STYLE["height"] = "6000px"
 STYLE["width"] = 1000
