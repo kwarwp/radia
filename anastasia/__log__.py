913d557282552dfc933f66483d42ce339ce21382
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 22:29:00 -0300

    Saved anastasia/__log__.py @2023-10-11 01:28

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -1582,3 +1582,23 @@ AttributeError: 'Peao' object has no attribute 'move'
     self.peao = Elemento(PAWN, x=20, y=70, w=80, h=80,
 AttributeError: 'Peao' object has no attribute 'move'
 '''},
+{'date': 'Tue Oct 10 2023 22:28:59.308 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 129
+    IlhaProibida()
+  module <module> line 59
+    self.peao = Peao(self)
+  module <module> line 89
+    self.peao = Elemento(PAWN, x=20, y=70, w=80, h=80,
+AttributeError: 'Peao' object has no attribute 'move'
+'''},
