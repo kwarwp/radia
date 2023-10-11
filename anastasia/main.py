9d8a94f7ddd28edfa2595ffdb4fbb523c4a3a51e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-10 22:30:19 -0300

    Saved anastasia/main.py @2023-10-11 01:30

--- anastasia/main.py
+++ anastasia/main.py
@@ -52,8 +52,8 @@ class IlhaProibida:  # : significa inicio de um bloco então posteriormente tem 
         #info_terrenos= [(10,PORTAO_BRONZE), (120,PALACIO_CORAL),(230,PORTAO_BRONZE)] #PAR ORDENADOS
         #info_terrenos = [PORTAO_BRONZE, PALACIO_CORAL]
         #info_terrenos = np.array([[0,1,0,1],[0,1,0,1],[0,1,0,1]])
-        info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL, OBSERVATORIO, BOSQUE, PORTAO_BRONZE,
-                        PORTAO_BRONZE, PORTAO_BRONZE]
+        info_terrenos= [[PORTAO_BRONZE, PORTAO_BRONZE, PORTAO_BRONZE, PORTAO_BRONZE], 
+                        [PORTAO_BRONZE, PORTAO_BRONZE, PORTAO_BRONZE, PORTAO_BRONZE]]
         self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
         for px, lc in enumerate(info_terrenos)]
         self.peao = Peao(self)
