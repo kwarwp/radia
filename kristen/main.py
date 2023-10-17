df932b5145110b919306ea7878528e060718d12e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-17 16:53:50 -0300

    Saved kristen/main.py @2023-10-17 19:53

--- kristen/main.py
+++ kristen/main.py
@@ -7,6 +7,5 @@ ROBIO = [4,6]
 img =".i.RB1._ROBIO.0"
 index = [4,6]
 [mapa_ROBIO]
-#p_bote = {img=".i.PUZZLE.AdvBo0.4.4", pr=1, foi=".i.ROOTzz0.0.0" , texto = "Conseguiu embarcar no bote!", cena=".i.RB1._ROBIO.4", dim=[4,4]}
 Jb = {local=[0,0], tit="Testando extra", texto="extra!", foi=".i.ROBIOzz0zz1.0.0"}
 
