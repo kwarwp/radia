c7ba1a8468ce52785d5ee9d50651db42529c8c04
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-17 16:32:31 -0300

    Created kristen/main.py @2023-10-17 19:32

---
+++ kristen/main.py
@@ -0,0 +1,12 @@
+# radia.kristen.main.py
+[valor_RB1]
+ROBIO = "https://i.imgur.com/IMcwSAq.jpg"
+[folha_RB1]
+ROBIO = [4,6]
+[l_ROBIO]
+img =".i.RB1._ROBIO.0"
+index = [4,6]
+[mapa_ROBIO]
+#p_bote = {img=".i.PUZZLE.AdvBo0.4.4", pr=1, foi=".i.ROOTzz0.0.0" , texto = "Conseguiu embarcar no bote!", cena=".i.RB1._ROBIO.4", dim=[4,4]}
+Jb = {local=[3,0], tit="Vamos desembarcar do navio", texto="Desembarcar!", foi=".i.bote"}
+
