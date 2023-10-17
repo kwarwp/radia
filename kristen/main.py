c02a89671e511cc80c469ae816f285ce00ba3974
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-17 17:16:41 -0300

    Saved kristen/main.py @2023-10-17 20:16

--- kristen/main.py
+++ kristen/main.py
@@ -3,9 +3,4 @@
 ROBIO = "https://i.imgur.com/IMcwSAq.jpg"
 [folha_RB1]
 ROBIO = [4,6]
-[l_ROBIO]
-img =".i.RB1._ROBIO.0"
-index = [4,6]
-[mapa_ROBIO]
-Jb = {local=[0,0], tit="Testando extra", texto="extra!", foi=".i.ROBIOzz0zz1.0.0"}
 
