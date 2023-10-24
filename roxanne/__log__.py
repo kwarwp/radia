65a1902d52df86387dfa80d5b0d8fc6bae15deee
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 12:58:27 -0300

    Saved roxanne/__log__.py @2023-10-24 15:58

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -416,3 +416,23 @@ TypeError: mover() takes 2 positional arguments but more were given
     self.peao.mover(self.terreno[0])
 AttributeError: 'IlhaProibida' object has no attribute 'terreno'
 '''},
+{'date': 'Tue Oct 24 2023 12:58:26.563 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 126
+    IlhaProibida()
+  module <module> line 49
+    self.peao.mover(self.terrenos[0])
+  module <module> line 115
+    self.terreno.desocupa_e_vai_para(terreno_destino)
+TypeError: desocupa_e_vai_para() takes 1 positional argument but more were given
+'''},
