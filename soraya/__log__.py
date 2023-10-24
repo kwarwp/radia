2bb3a2ad1fae85301ca1edb02ca5a431b9097ea5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:42:45 -0300

    Saved soraya/__log__.py @2023-10-24 13:42

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -452,3 +452,23 @@ SyntaxError: invalid syntax
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Oct 24 2023 10:42:45.908 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 112
+    IlhaProibida()
+  module <module> line 47
+    self.monta_tabuleiro_oceano()
+  module <module> line 56
+    if(px==py):
+UnboundLocalError: local variable 'px' referenced before assignment
+'''},
