6dea55326e23c85f17940c802d1f8716c9d8d8d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 12:09:11 -0300

    Saved roxanne/__log__.py @2023-10-24 15:09

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -378,3 +378,23 @@ IndentationError
 File "anastasia.main", line 78
     info_terrenos = [
 '''},
+{'date': 'Tue Oct 24 2023 12:09:10.552 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 114
+    IlhaProibida()
+  module <module> line 49
+    self.terrenos[1].ocupa(self.peao)
+  module <module> line 81
+    peao.mover(self.posx, self)
+TypeError: mover() takes 2 positional arguments but more were given
+'''},
