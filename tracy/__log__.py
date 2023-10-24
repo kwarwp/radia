a613791f53bc2fbf64b73fdcc848e7bab8635c3d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:51:08 -0300

    Saved tracy/__log__.py @2023-10-24 13:51

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -48,3 +48,10 @@ SyntaxError: invalid syntax
                                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 24 2023 10:50:47.707 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 67
+  """def direita(self, terreno):"""
+  ^
+IndentationError: unexpected indent
+'''},
