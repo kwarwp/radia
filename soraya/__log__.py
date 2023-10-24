8ffb6f85aa32e6fda44aca444375ff0226fa962e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 13:05:14 -0300

    Saved soraya/__log__.py @2023-10-24 16:05

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -1147,3 +1147,21 @@ TypeError: mover() takes 2 positional arguments but more were given
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Oct 24 2023 13:05:14.440 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 116
+    IlhaProibida()
+  module <module> line 48
+    self.terrenos[1].ocupa(self.peao)
+IndexError: list index out of range
+'''},
