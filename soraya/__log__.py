a4dac33a09efb3ded07a3d046c5b386703366563
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:28:39 -0300

    Saved soraya/__log__.py @2023-10-24 13:28

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -249,3 +249,21 @@ AttributeError: 'list' object has no attribute 'ocupa'
     self.terrenos[1].ocupa(self.peao)
 AttributeError: 'list' object has no attribute 'ocupa'
 '''},
+{'date': 'Tue Oct 24 2023 10:28:40.5 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 110
+    IlhaProibida()
+  module <module> line 49
+    self.terrenos[1].ocupa(self.peao)
+AttributeError: 'list' object has no attribute 'ocupa'
+'''},
