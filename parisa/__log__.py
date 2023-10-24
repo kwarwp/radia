8a2e796e8c76273412b55747285567f258b986d8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:43:57 -0300

    Created parisa/__log__.py @2023-10-24 13:43

---
+++ parisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Oct 24 2023 10:43:55.72 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 42
+  n = 5
+  ^
+IndentationError: unexpected indent
+'''},
