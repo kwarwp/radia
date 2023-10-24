af73d466d6487b7396ef73e1758b19ff8e3de1a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 11:54:23 -0300

    Saved julia/__log__.py @2023-10-24 14:54

--- julia/__log__.py
+++ julia/__log__.py
@@ -751,3 +751,23 @@ AttributeError: 'Peao' object has no attribute 'terrenos'
     self.terreno = self.terrenos.index[1]
 AttributeError: 'Peao' object has no attribute 'terrenos'
 '''},
+{'date': 'Tue Oct 24 2023 11:54:23.155 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 115
+    IlhaProibida()
+  module <module> line 47
+    self.peao = Peao(self)
+  module <module> line 90
+    self.terreno = self.terrenos.index[1]
+AttributeError: 'Peao' object has no attribute 'terrenos'
+'''},
