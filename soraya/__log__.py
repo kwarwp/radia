51519f89f8cb1521f6d793bd664118afceb1a291
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 11:42:54 -0300

    Saved soraya/__log__.py @2023-10-24 14:42

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -851,3 +851,23 @@ AttributeError: 'list' object has no attribute 'ocupa'
     self.terrenos[1].ocupa(self.peao)
 AttributeError: 'list' object has no attribute 'ocupa'
 '''},
+{'date': 'Tue Oct 24 2023 11:42:54.645 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    from anastasia import main as amain
+ImportError: cannot import name 'main'
+
+SyntaxError
+File "anastasia.main", line 82
+    self.terrenos = [Terreno(cena=oceano, posy=py*110+10, posx=(6-len(row))*55, posx=px*110+10,
+'''},
