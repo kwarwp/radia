4bf23566af13b4e46770510132ca3af4a3fc69a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 12:38:41 -0300

    Saved soraya/__log__.py @2023-10-24 15:38

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -1058,3 +1058,25 @@ NameError: name 'ilha' is not defined
     self.peao, self.ilha = None, ilha
 NameError: name 'ilha' is not defined
 '''},
+{'date': 'Tue Oct 24 2023 12:38:42.168 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 113
+    IlhaProibida()
+  module <module> line 46
+    self.monta_tabuleiro_oceano()
+  module <module> line 55
+    self.terrenos = [Terreno(cena=oceano, posy=px//6*110 + 50, posx=((px%6)+int(abs(2.5-px//6)))*110+10, local=lc)
+  module <module> line 72
+    self.peao, self.ilha = None, ilha
+NameError: name 'ilha' is not defined
+'''},
