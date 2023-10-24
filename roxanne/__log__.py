7bd7436055ee973ded5dd75756bff2ba2d11d797
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:45:50 -0300

    Saved roxanne/__log__.py @2023-10-24 13:45

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -327,3 +327,10 @@ SyntaxError: invalid syntax
                                                                                                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 24 2023 10:45:49.558 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 57
+  self.terrenos = [Terreno(cena=oceano, posy=((px//6)+int(abs(2.5-px//6)) * 110 + 50, posx=(px%6)*110+10, local=lc)
+                                                                                                                   ^
+SyntaxError: invalid syntax
+'''},
