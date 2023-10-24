2bd1f7fdb409a33677778f8c9f01a2a3847d5b0a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:19:27 -0300

    Saved rachel/__log__.py @2023-10-24 13:19

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -189,3 +189,23 @@ SyntaxError: invalid syntax
                                                                                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 24 2023 10:19:26.827 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 107
+    IlhaProibida()
+  module <module> line 44
+    self.monta_tabuleiro_oceano()
+  module <module> line 53
+    for y, ilc in enumerate(info_terrnos):
+NameError: name 'info_terrnos' is not defined
+'''},
