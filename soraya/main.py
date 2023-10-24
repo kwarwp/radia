0e6c9344120e953483c414abd41eb31df681b105
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 11:12:42 -0300

    Saved soraya/main.py @2023-10-24 14:12

--- soraya/main.py
+++ soraya/main.py
@@ -55,7 +55,8 @@ class IlhaProibida:
         info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE]
         self.terrenos = [[Terreno(cena=oceano, posy=py*110+50, posx=px*110+10, local=lc)
                          for px, lc in enumerate(info_terrenos)]
-                         	for py, lc in enumerate(info_terrenos)]
+                         	for py, lc in enumerate(info_terrenos)
+                            	if px*py >2]
         
     def direita(self, terreno):
         """ Move o peão para a direita.
