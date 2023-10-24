d37face15b7e98c0f002a2b6f324a360eb99e2df
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:00:28 -0300

    Saved julia/__log__.py @2023-10-24 13:00

--- julia/__log__.py
+++ julia/__log__.py
@@ -484,3 +484,19 @@ SyntaxError: invalid syntax
     from julia.main import IlhaProibida as Ilha
 ImportError: cannot import name 'IlhaProibida'
 '''},
+{'date': 'Tue Oct 24 2023 10:00:27.618 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    from julia.main import IlhaProibida as Ilha
+ImportError: cannot import name 'IlhaProibida'
+'''},
