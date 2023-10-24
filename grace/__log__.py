9ece078ac806ad46d95fe065f6ab7441691202c0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:14:07 -0300

    Saved grace/__log__.py @2023-10-24 13:14

--- grace/__log__.py
+++ grace/__log__.py
@@ -399,3 +399,21 @@ NameError: free variable 'row' referenced before assignment in enclosing scope
     info_terrenos = [
 NameError: name 'NONE' is not defined
 '''},
+{'date': 'Tue Oct 24 2023 10:14:07.64 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 112
+    IlhaProibida()
+  module <module> line 48
+    self.terrenos[1][1].ocupa(self.peao)
+AttributeError: 'Terreno' object has no attribute '__getitem__'
+'''},
