26480ea7fbbdaeb3afe5c08ae91cb18935234b41
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 11:10:34 -0300

    Saved julia/__log__.py @2023-10-24 14:10

--- julia/__log__.py
+++ julia/__log__.py
@@ -619,3 +619,23 @@ Traceback (most recent call last):
   module anastasia.main line 35
     from _spy.circus.game import Circus
 '''},
+{'date': 'Tue Oct 24 2023 11:10:33.942 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 117
+    IlhaProibida()
+  module <module> line 46
+    self.monta_tabuleiro_oceano()
+  module <module> line 61
+    self.terrenos = [Terreno(local=lc, posy=py, posx=px*110+10,cena=oceano)]
+NameError: name 'lc' is not defined
+'''},
