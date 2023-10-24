4477558b9edbca3daffc5dea9c6a5621fd0979ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:48:54 -0300

    Saved anastasia/__log__.py @2023-10-24 13:48

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -1940,3 +1940,10 @@ Traceback (most recent call last):
     self.peao = Elemento(PAWN, x=20, y=70, w=80, h=80,
 AttributeError: 'Peao' object has no attribute 'move'
 '''},
+{'date': 'Tue Oct 24 2023 10:48:53.1 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 80
+  self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
+                                                                                                                                                                                                                             ^
+SyntaxError: invalid syntax
+'''},
