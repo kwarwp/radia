4f41757b97972556c1ec03da4b18857a90d7947b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:16:32 -0300

    Saved grace/__log__.py @2023-10-24 13:16

--- grace/__log__.py
+++ grace/__log__.py
@@ -435,3 +435,21 @@ AttributeError: 'Terreno' object has no attribute '__getitem__'
     self.monta_tabuleiro_oceano(self)
 TypeError: monta_tabuleiro_oceano() takes 1 positional argument but more were given
 '''},
+{'date': 'Tue Oct 24 2023 10:16:31.759 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 112
+    IlhaProibida()
+  module <module> line 48
+    self.terrenos[1,1].ocupa(self.peao)
+TypeError: list indices must be integer, not tuple
+'''},
