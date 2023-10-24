c4346bf2db75b019b99b8eda1608d435802c4daf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 09:58:32 -0300

    Saved julia/main.py @2023-10-24 12:58

--- julia/main.py
+++ julia/main.py
@@ -20,6 +20,9 @@ Changelog
 """
 
 from _spy.vitollino.main import Cena, Elemento, STYLE
+from julia.main import IlhaProibida as Ilha
+from julia import main as jmain
+from anastasia import main as amain
 STYLE["width"] = 800
 STYLE["height"] = "600px"
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
@@ -29,8 +32,11 @@ PAWN = "https://imgur.com/zO3kiRp.png"
 
 
 class IlhaProibida:
-    """ Representa a classe principal do jogo.
-       Terrenos: locais onde os peões ficam posicionados.
+    """ Representa a classe principal do Jogo.
+    
+    Terrenos 
+        Locais onde os peões podem ficar.
+        
     """
     def __init__(self):
         self.oceano = oceano = Cena(IMAGEM).vai()
@@ -38,30 +44,24 @@ class IlhaProibida:
         #cena=oceano)
         #self.terreno1 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
         #info_terrenos= [(10, PORTAO_BRONZE), (120, PALACIO_CORAL), (230, PORTAO_BRONZE)]
-        info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL, 
-                        PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL, 
-                        PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL]
-        self.terrenos=[]
-        px=1
-        py=50
-        for i in range (0,11):
-            if i==3:
-               py = py+100
-               px=1
-            if x==7:
-               py = py+100
-               px = 1
-            self.terrenos += [Terreno(cena=oceano, posy=py, posx=px*110+10, local=info_terrenos[i])]
-            px+=1
-            
-     #   self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
-    #    for px, lc in enumerate(info_terrenos), ]
+        self.terrenos = []
+        self.monta_tabuleiro_oceano()
         self.peao = Peao(self)
-        self.terrenos[0].ocupa(self.peao)
+        self.terrenos[1].ocupa(self.peao)
+        
+    def monta_tabuleiro_oceano(self):
+        """ Montar o tabuleiro em forma de diamante.
+        
+        """
+        info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL] * 6
+        self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
+                         for px, lc in enumerate(info_terrenos)]
         
     def direita(self, terreno):
-        """ Move o peão para a direita
+        """ Move o peão para a direita.
+        
         :param terreno: O terreno onde está o peão
+        :return: O terreno onde o peão vai
         """
         aqui = self.terrenos.index(terreno)
         return self.terrenos[aqui+1]
@@ -78,20 +78,34 @@ class Terreno:
         peao.mover(self.posx, self)
 
 class Peao:
+    """ Marcador usado para definir a posição do jogador nos terrenos.
+    """
     def __init__(self, ilha):
         self.peao = Elemento(PAWN, x=20, y=70, w=80, h= 80,
         cena=ilha.oceano, vai=self.move)
         self.terreno = None
         self.ilha = ilha
+        #self.peao.vai = self.move
+        
+    def move_(self, ev=None):  # Corrigir: não está condizente!
+        terreno_destino = self.ilha.direita(0) #(self.terreno)
+        self.peao.y = 300
         
     def move(self, ev=None):  # Corrigir: não está condizente!
         terreno_destino = self.ilha.direita(self.terreno)
-        terreno_destino.ocupa(self)  
-        
+        #self.peao.x = 170
+        terreno_destino.ocupa(self)        
     def mover(self, x, terreno):
         self.terreno = terreno
         self.peao.x = x
+
         
-if __name__ == "__main__":
-    print ("Executado no modulo julia")
+if __name__ == "__main__": 
+    #print((amain.__name__))
+    ##import antigravity as ag
+    #from __future__ import braces
+    import __phello__
+    #ag.fly()
+    
     IlhaProibida()
+    #IlhaProibida()
