f1112beb4ed85c3e333251300efaeaafb9656a5c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 13:18:52 -0300

    Saved roxanne/__log__.py @2023-10-24 16:18

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -436,3 +436,23 @@ AttributeError: 'IlhaProibida' object has no attribute 'terreno'
     self.terreno.desocupa_e_vai_para(terreno_destino)
 TypeError: desocupa_e_vai_para() takes 1 positional argument but more were given
 '''},
+{'date': 'Tue Oct 24 2023 13:18:51.620 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 132
+    IlhaProibida()
+  module <module> line 47
+    self.monta_tabuleiro_oceano()
+  module <module> line 60
+    self.terreno[4].afunda()
+AttributeError: 'IlhaProibida' object has no attribute 'terreno'
+'''},
