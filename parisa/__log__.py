1cf862a7a2e044a5c92d20abf9ffd67f2dade5f1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:45:25 -0300

    Saved parisa/__log__.py @2023-10-24 13:45

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Oct 24 2023 10:45:24.618 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 42
+  w = 5
+  ^
+IndentationError: unexpected indent
+'''},
