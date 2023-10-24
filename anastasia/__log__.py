282f028fc3d8fc5f44c78853e8e41ebbd54dc093
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 11:01:19 -0300

    Saved anastasia/__log__.py @2023-10-24 14:01

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -2084,3 +2084,23 @@ AttributeError: 'IlhaProibida' object has no attribute 'sprite'
     info_terrenos = self.sprite("terreno", 0, 0)
 AttributeError: 'IlhaProibida' object has no attribute 'sprite'
 '''},
+{'date': 'Tue Oct 24 2023 11:01:18.52 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 172
+    IlhaProibida()
+  module <module> line 69
+    self.monta_tabuleiro_oceano()
+  module <module> line 93
+    info_terrenos = self.sprite("terreno", 0, 0)
+AttributeError: 'IlhaProibida' object has no attribute 'sprite'
+'''},
