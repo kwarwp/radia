a6848384c1e9b0fb8774257fdbc3c3a545d9f14c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 12:43:15 -0300

    Saved soraya/__log__.py @2023-10-24 15:43

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -1100,3 +1100,23 @@ NameError: name 'ilha' is not defined
     self.terrenos = [Terreno(cena=self.oceano, posy=px//6, posx=((px%6)+int(abs(2.5-px//6))), local=lc, ilha=self)
 TypeError: __init__() got an unexpected keyword argument 'ilha'
 '''},
+{'date': 'Tue Oct 24 2023 12:43:15.663 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 113
+    IlhaProibida()
+  module <module> line 48
+    self.terrenos[1].ocupa(self.peao)
+  module <module> line 81
+    peao.mover(self.posx, self)
+TypeError: mover() takes 2 positional arguments but more were given
+'''},
