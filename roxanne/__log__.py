05e161534e802c47f83a2363e3f66f7db1b4b15a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 11:43:57 -0300

    Saved roxanne/__log__.py @2023-10-24 14:43

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -358,3 +358,23 @@ NameError: name 'x' is not defined
 'error': '''
 Exception: <TypeError: Cannot read properties of undefined (reading 'type')>
 '''},
+{'date': 'Tue Oct 24 2023 11:43:56.924 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    from anastasia import main as amain
+ImportError: cannot import name 'main'
+
+IndentationError
+File "anastasia.main", line 78
+    info_terrenos = [
+'''},
