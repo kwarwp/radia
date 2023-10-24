c0c0e4be7eaeb23271e3b04fabcc3c15c49c4afe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 11:12:35 -0300

    Saved julia/__log__.py @2023-10-24 14:12

--- julia/__log__.py
+++ julia/__log__.py
@@ -639,3 +639,21 @@ Traceback (most recent call last):
     self.terrenos = [Terreno(local=lc, posy=py, posx=px*110+10,cena=oceano)]
 NameError: name 'lc' is not defined
 '''},
+{'date': 'Tue Oct 24 2023 11:12:34.910 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 117
+    IlhaProibida()
+  module <module> line 48
+    self.terrenos[1].ocupa(self.peao)
+IndexError: list index out of range
+'''},
