9f80e1856a67d8574c119183abe454af9da0b947
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 11:44:24 -0300

    Saved soraya/__log__.py @2023-10-24 14:44

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -964,3 +964,21 @@ Traceback (most recent call last):
   module <module> line 25
     from anastasia import main as amain
 '''},
+{'date': 'Tue Oct 24 2023 11:44:24.792 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 117
+    IlhaProibida()
+  module <module> line 48
+    self.terrenos[1].ocupa(self.peao)
+IndexError: list index out of range
+'''},
