5b3f4adc4129b0c12a415c8f601ce9fc2cad1ec3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 11:42:57 -0300

    Saved soraya/__log__.py @2023-10-24 14:42

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -871,3 +871,34 @@ SyntaxError
 File "anastasia.main", line 82
     self.terrenos = [Terreno(cena=oceano, posy=py*110+10, posx=(6-len(row))*55, posx=px*110+10,
 '''},
+{'date': 'Tue Oct 24 2023 11:42:58.99 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    from anastasia import main as amain
+ImportError: cannot import name 'main'
+
+AttributeError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    from anastasia import main as amain
+'''},
