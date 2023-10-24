fca1becd0ec3dd70de817877259521f67e8b6569
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:06:12 -0300

    Saved soraya/main.py @2023-10-24 13:06

--- soraya/main.py
+++ soraya/main.py
@@ -19,6 +19,9 @@ Changelog
 """
 
 from _spy.vitollino.main import Cena, Elemento, STYLE
+from julia.main import IlhaProibida as Ilha
+from julia import main as jmain
+from anastasia import main as amain
 STYLE["width"] = 800
 STYLE["height"] = "600px"
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
@@ -34,19 +37,24 @@ class IlhaProibida:
         Locais onde os peões podem ficar.
         
     """
-    #INCREMENTAR A ILHA 4x3 TERRENOS
-    
     def __init__(self):
         self.oceano = oceano = Cena(IMAGEM).vai()
         #self.terreno = Terreno(PORTAO_BRONZE, posx=10, posy=50,
         #cena=oceano)
         #self.terreno1 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
         #info_terrenos= [(10, PORTAO_BRONZE), (120, PALACIO_CORAL), (230, PORTAO_BRONZE)]
-        info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL]
-        self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
-        for px, lc in enumerate(info_terrenos)]
+        self.terrenos = []
+        self.monta_tabuleiro_oceano()
         self.peao = Peao(self)
-        self.terrenos[0].ocupa(self.peao)
+        self.terrenos[1].ocupa(self.peao)
+        
+    def monta_tabuleiro_oceano(self):
+        """ Montar o tabuleiro em forma de diamante.
+        
+        """
+        info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL] * 6
+        self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
+                         for px, lc in enumerate(info_terrenos)]
         
     def direita(self, terreno):
         """ Move o peão para a direita.
@@ -76,9 +84,13 @@ class Peao:
         cena=ilha.oceano, vai=self.move)
         self.terreno = None
         self.ilha = ilha
+        #self.peao.vai = self.move
         
+    def move_(self, ev=None):  # Corrigir: não está condizente!
+        terreno_destino = self.ilha.direita(0) #(self.terreno)
+        self.peao.y = 300
         
-    def move(self, ev=None):  
+    def move(self, ev=None):  # Corrigir: não está condizente!
         terreno_destino = self.ilha.direita(self.terreno)
         #self.peao.x = 170
         terreno_destino.ocupa(self)        
@@ -87,7 +99,12 @@ class Peao:
         self.peao.x = x
 
         
-if __name__ == "__main__":
-    #print((jmain.__name__))
-    #print(help(Ilha))
+if __name__ == "__main__": 
+    #print((amain.__name__))
+    ##import antigravity as ag
+    #from __future__ import braces
+    import __phello__
+    #ag.fly()
+    
     IlhaProibida()
+    #IlhaProibida()
