e55dc2563e49b352ffc02dfd51a62703e5e6a4f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:04:39 -0300

    Saved roxanne/__log__.py @2023-10-24 13:04

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -255,3 +255,19 @@ Traceback (most recent call last):
   module julia.main line 23
     from julia.main import IlhaProibida as Ilha
 '''},
+{'date': 'Tue Oct 24 2023 10:04:38.193 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 22
+    from julia.main import IlhaProibida as Ilha
+ImportError: cannot import name 'IlhaProibida'
+'''},
