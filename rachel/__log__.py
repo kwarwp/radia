16379d65f4566820c245749717d51c073ca5406b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:03:47 -0300

    Saved rachel/__log__.py @2023-10-24 13:03

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -114,3 +114,23 @@ NameError: name 'py' is not defined
     self.terrenos = [Terreno(cena=oceano, posy=py*110+10, posx=px*110+10, local=lc)
 NameError: name 'py' is not defined
 '''},
+{'date': 'Tue Oct 24 2023 10:03:46.823 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 106
+    IlhaProibida()
+  module <module> line 44
+    self.monta_tabuleiro_oceano()
+  module <module> line 53
+    self.terrenos = [Terreno(cena=oceano, posy=py*110+10, posx=px*110+10, local=lc)
+NameError: name 'py' is not defined
+'''},
