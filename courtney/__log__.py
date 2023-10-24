b04497c01121ddf48f57a1d2cdfbe9be0406d6df
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:49:43 -0300

    Saved courtney/__log__.py @2023-10-24 13:49

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -17,3 +17,21 @@
     self.monta_tabuleiro_oceano[]
 AttributeError: 'method' object has no attribute '__getitem__'
 '''},
+{'date': 'Tue Oct 24 2023 10:49:42.996 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 109
+    IlhaProibida()
+  module <module> line 47
+    self.monta_tabuleiro_oceano[]
+AttributeError: 'method' object has no attribute '__getitem__'
+'''},
