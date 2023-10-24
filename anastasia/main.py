d885cfb20ea5c68481e1fe8921a9515b28603a2c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:45:35 -0300

    Saved anastasia/main.py @2023-10-24 13:45

--- anastasia/main.py
+++ anastasia/main.py
@@ -66,7 +66,7 @@ class IlhaProibida:  # : significa inicio de um bloco então posteriormente tem 
        # shuffle(info_terrenos)
         #self.terrenos = [Terreno(cena=oceano, posy=50, posx=px*110+10, local=lc)
         self.terrenos = []
-         self.monta_tabuleiro_oceano()
+        self.monta_tabuleiro_oceano()
         self.peao = Peao(self)
         self.terrenos[1].ocupa(self.peao)#chamou o terreno 1 e ocupa com o peão
         #for px, lc in enumerate(info_terrenos)]
