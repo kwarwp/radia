5218884984289ba86d616d0e3c100b7173cb1fe8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:21:13 -0300

    Saved rachel/main.py @2023-10-24 13:21

--- rachel/main.py
+++ rachel/main.py
@@ -51,7 +51,9 @@ class IlhaProibida:
         """
         info_terrenos= [[PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL] * 6]
         for y, ilc in enumerate(info_terrenos):
+            print("y", y)
             for x, lc in enumerate(ilc):
+                print("x", x)
                 self.terrenos.append(Terreno(cena=oceano, posy=y*110+10, posx=x*110+10, local=lc))
         
     def direita(self, terreno):
