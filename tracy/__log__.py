3f966710e1111dcf322db99ca8b1846cbe47bfa3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 10:14:59 -0300

    Created tracy/__log__.py @2023-10-24 13:14

---
+++ tracy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Oct 24 2023 10:14:38.183 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 56
+  \n
+   ^
+SyntaxError: invalid syntax
+'''},
