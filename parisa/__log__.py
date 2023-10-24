9e4093590c3fc9dbc65e9f0c50fc957e61663279
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-24 13:04:09 -0300

    Saved parisa/__log__.py @2023-10-24 16:04

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -13,3 +13,21 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Oct 24 2023 13:04:06.830 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 98
+    IlhaProibida()
+  module <module> line 32
+    self.peao.mover(self.terreno[0])
+AttributeError: 'IlhaProibida' object has no attribute 'terreno'
+'''},
