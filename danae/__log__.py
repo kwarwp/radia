b37922d60af796754b38ec538d145489d4ed77c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-30 17:03:08 -0300

    Saved danae/__log__.py @2023-10-30 20:03

--- danae/__log__.py
+++ danae/__log__.py
@@ -247,3 +247,23 @@ SyntaxError: invalid syntax
                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Oct 30 2023 17:03:07.776 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 143
+    IlhaProibida()
+  module <module> line 76
+    self.monta_tabuleiro_oceano()
+  module <module> line 101
+    spl = sample(8, list(range(24)))
+TypeError: Population must be a sequence or set.  For dicts, use list(d).
+'''},
