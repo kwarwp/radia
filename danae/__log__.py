8b71a3d93895935c6df7ef4765aa39a2a868364c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-30 10:57:17 -0300

    Saved danae/__log__.py @2023-10-30 13:57

--- danae/__log__.py
+++ danae/__log__.py
@@ -19,3 +19,25 @@
     self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
 NameError: name 'Terreno' is not defined
 '''},
+{'date': 'Mon Oct 30 2023 10:57:16.735 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 78
+    IlhaProibida()
+  module <module> line 42
+    self.monta_tabuleiro_oceano()
+  module <module> line 51
+    self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
+  module <module> line 72
+    self.local.vai = self.vai
+AttributeError: 'Terreno' object has no attribute 'vai'
+'''},
