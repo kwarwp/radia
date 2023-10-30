a169444cd88ee567ac92a41e11e57551829241a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-30 13:16:25 -0300

    Saved danae/__log__.py @2023-10-30 16:16

--- danae/__log__.py
+++ danae/__log__.py
@@ -144,3 +144,23 @@ TypeError: sequence item 0: expected str instance, list found
    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Oct 30 2023 13:16:24.576 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 125
+    IlhaProibida()
+  module <module> line 73
+    self.monta_tabuleiro_oceano()
+  module <module> line 82
+    info_terrenos = it = [
+NameError: name 'PORTAO_OURO' is not defined
+'''},
