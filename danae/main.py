1dd075b8eb255e72e138f77ffbc8c0f901751eaa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-30 12:38:33 -0300

    Saved danae/main.py @2023-10-30 15:38

--- danae/main.py
+++ danae/main.py
@@ -21,31 +21,32 @@ from _spy.vitollino.main import Cena, Elemento, STYLE
 
 STYLE["width"] = 800
 STYLE["height"] = "600px"
-PORTAO_OURO = "https://i.imgur.com/J6ow4jR.jpg"
-IMAGEM = "https://imgur.com/gVHmY2v.jpg"
-PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
-PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
-VALE_TENEBROSO = "https://i.imgur.com/OZE1myn.jpg"
 PORTA_OURO = "https://i.imgur.com/PvkZSQP.jpg"
-PORTAO_PRATA = "https://i.imgur.com/v0g7eGm.jpg"
-PORTAO_COBRE = "https://i.imgur.com/45aU3nf.jpg"
-PORTAO_FERRO = "https://i.imgur.com/yKU6ngz.jpg"
-ATALAIA = "https://i.imgur.com/sdJ4W5O.jpg"
-JARDIM_SUSSUROS = "https://i.imgur.com/pjVcyoy.jpg"
-PISTA_POUSO = "https://i.imgur.com/CU3TLYh.png"
-JARDIM_UIVOS = "https://i.imgur.com/ZNuPWqZ.jpg"
-TEMPLO_SOL = "https://i.imgur.com/O0OSVFt.jpg"
-TEMPLO_LUA = "https://i.imgur.com/J160xpm.jpg"
-CAVERNA_LAVA = "https://i.imgur.com/2j1IAyf.jpg"
-CAVERNA_SOMBRAS = "https://i.imgur.com/b4xtltc.png"
-OBSERVATORIO = "https://i.imgur.com/E9MflTP.jpg"
-PANTANO_BRUMAS = "https://i.imgur.com/NDioDZg.jpg"
-ROCHA_FANTASMA = "https://i.imgur.com/TCmLjeT.png"
-PALACIO_MARES = "https://i.imgur.com/rYxQaTa.png"
-PENHASCO_ABANDONO = "https://i.imgur.com/MvN7kTU.jpg"
-BOSQUE_CARMESIM = "https://i.imgur.com/Uni02EK.jpg"
-DUNAS_ENGANO = "https://i.imgur.com/cG5UYCf.jpg"
-PONTE_SUSPENSA = "https://i.imgur.com/GC8V8CQ.jpg"
+IMAGEM = "https://imgur.com/gVHmY2v.jpg"
+PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"#
+PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"#
+VALE_TENEBROSO = "https://i.imgur.com/OZE1myn.jpg"#
+PORTAO_OURO = "https://i.imgur.com/J6ow4jR.jpg"#
+PORTAO_PRATA = "https://i.imgur.com/v0g7eGm.jpg"#
+PORTAO_COBRE = "https://i.imgur.com/45aU3nf.jpg"#
+PORTAO_FERRO = "https://i.imgur.com/yKU6ngz.jpg"#
+ATALAIA = "https://i.imgur.com/sdJ4W5O.jpg"#
+JARDIM_SUSSUROS = "https://i.imgur.com/pjVcyoy.jpg"#
+PISTA_POUSO = "https://i.imgur.com/CU3TLYh.png"#
+JARDIM_UIVOS = "https://i.imgur.com/ZNuPWqZ.jpg"#
+TEMPLO_SOL = "https://i.imgur.com/O0OSVFt.jpg"#
+TEMPLO_LUA = "https://i.imgur.com/J160xpm.jpg"#
+CAVERNA_LAVA = "https://i.imgur.com/2j1IAyf.jpg"#
+CAVERNA_SOMBRAS = "https://i.imgur.com/b4xtltc.png"#
+OBSERVATORIO = "https://i.imgur.com/E9MflTP.jpg"#
+PANTANO_BRUMAS = "https://i.imgur.com/NDioDZg.jpg"#
+ROCHA_FANTASMA = "https://i.imgur.com/TCmLjeT.png"#
+PALACIO_MARES = "https://i.imgur.com/rYxQaTa.png"#
+PENHASCO_ABANDONO = "https://i.imgur.com/MvN7kTU.jpg"#
+BOSQUE_CARMESIM = "https://i.imgur.com/Uni02EK.jpg"#
+DUNAS_ENGANO = "https://i.imgur.com/cG5UYCf.jpg"#
+PONTE_SUSPENSA = "https://i.imgur.com/GC8V8CQ.jpg"#
+LAGOA_PERDIDA = "https://i.imgur.com/7o1qq10.png"#
 PAWN = "https://imgur.com/zO3kiRp.png"
 
 
@@ -70,9 +71,9 @@ class IlhaProibida:
         """
         info_terrenos = it = [
         PORTAO_OURO, PALACIO_CORAL, PORTAO_BRONZE, VALE_TENEBROSO, CAVERNA_LAVA, PORTAO_FERRO,
-        CAVERNA_SOMBRAS, OBSERVATORIO, PANTANO_BRUMAS, ROCHA_FANTASMA, PALACIO_MARES,
+        CAVERNA_SOMBRAS, OBSERVATORIO, PANTANO_BRUMAS, ROCHA_FANTASMA, PALACIO_MARES, JARDIM_SUSSUROS,
         PENHASCO_ABANDONO, BOSQUE_CARMESIM, DUNAS_ENGANO, PONTE_SUSPENSA, PORTAO_PRATA,
-        PORTAO_COBRE, ATALAIA, PISTA_POUSO, JARDIM_UIVOS, TEMPLO_SOL, TEMPLO_LUA] * 2
+        PORTAO_COBRE, ATALAIA, PISTA_POUSO, JARDIM_UIVOS, TEMPLO_SOL, TEMPLO_LUA, LAGOA_PERDIDA] * 2
         self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
                                  posx=((px % 6) + int(abs(2.5 - px // 6))), local=it.pop(), ilha=self)
                          for px, lc in enumerate(info_terrenos[:36]) if px % 6 < 6 - int(abs(2.5 - px // 6)) * 2]
