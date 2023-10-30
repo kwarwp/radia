c12c3320a26914d8e961be8fe8d0426ddeeba4bc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-30 16:06:06 -0300

    Saved danae/__log__.py @2023-10-30 19:06

--- danae/__log__.py
+++ danae/__log__.py
@@ -211,3 +211,25 @@ TypeError: 'str' object does not support item assignment
     self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
 IndexError: pop index out of range
 '''},
+{'date': 'Mon Oct 30 2023 16:06:05.885 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 131
+    IlhaProibida()
+  module <module> line 74
+    self.monta_tabuleiro_oceano()
+  module <module> line 92
+    self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
+  module <module> line 117
+    tit.elt.text = local.nome #"UM LOCAL QUALQUER"
+AttributeError: 'str' object has no attribute 'nome'
+'''},
