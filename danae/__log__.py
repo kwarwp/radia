a9a6130aaea1a0c8b09ef39bb74619293ccfbc4d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-30 17:19:20 -0300

    Saved danae/__log__.py @2023-10-30 20:19

--- danae/__log__.py
+++ danae/__log__.py
@@ -267,3 +267,23 @@ SyntaxError: invalid syntax
     spl = sample(8, list(range(24)))
 TypeError: Population must be a sequence or set.  For dicts, use list(d).
 '''},
+{'date': 'Mon Oct 30 2023 17:19:20.65 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 145
+    IlhaProibida()
+  module <module> line 76
+    self.monta_tabuleiro_oceano()
+  module <module> line 96
+    lc, ln, tf_ = locais[loc].nome
+ValueError: too many values to unpack (expected 3)
+'''},
