be734cc601c05819236fe57fa9f20aeed1e8990e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 11:34:22 -0300

    Saved sarah/main.py @2023-10-31 14:34

--- sarah/main.py
+++ sarah/main.py
@@ -33,6 +33,7 @@ class IlhaProibida:
         from random import shuffle
         # Agora info_terrenos será uma lista de Ter -> Como criar?
         info_terrenos = [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL] * 9
+        Ter = [info_terrenos]
         shuffle(info_terrenos)
         # Cada terreno realmente criado "puxa" um terreno da lista de "Ter's
         self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
