ed0735350bc8690bd1dffa28cc33272559443e2f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 12:36:48 -0300

    Saved anastasia/__log__.py @2023-10-31 15:36

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -2536,3 +2536,23 @@ SyntaxError: invalid syntax
                                                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 31 2023 12:36:29.394 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 208
+    IlhaProibida()
+  module <module> line 87
+    self.monta_tabuleiro_oceano()
+  module <module> line 98
+    info_terrenos = [Ter(nome=NOME.pop(0), imagem=LINKS.pop(0), tafv=tafv.pop()) for _ in range(24)]#Agora info_terrenos é uma lista de Ter #pop elimina o último
+NameError: name 'NOME' is not defined
+'''},
