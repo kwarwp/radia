5c9c81c00e3474ab9f236eed890c6be423ac953f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 12:30:56 -0300

    Saved roxanne/__log__.py @2023-10-31 15:30

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -547,3 +547,10 @@ SyntaxError: invalid syntax
                                                                                                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 31 2023 11:54:03.830 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 62
+  info_terrenos = it = [Ter(nome=NOMES.pop(0), imagem=LINKS.pop(0),
+                                                                                                              ^
+SyntaxError: invalid syntax
+'''},
