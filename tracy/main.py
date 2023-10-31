0342c7a650ab1cbd12d0ee9e96cbc0d91d64a72f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 12:37:56 -0300

    Saved tracy/main.py @2023-10-31 15:37

--- tracy/main.py
+++ tracy/main.py
@@ -35,7 +35,7 @@ NOMES = ("PISTA_POUSO PORTAO_BRONZE PALACIO_CORAL VALE_TENEBROSO PORTAO_OURO POR
 LINKS = ("CU3TLYh BL6lB7H tLDbzd2 OZE1myn J6ow4jR v0g7eGm 45aU3nf "
 "yKU6ngz sdJ4W5O pjVcyoy ZNuPWqZ O0OSVFt "
 "J160xpm 2j1IAyf b4xtltc E9MflTP NDioDZg TCmLjeT "
-"rYxQaTa MvN7kTU Uni02EK cG5UYCf GC8V8CQ 7o1qq10").split
+"rYxQaTa MvN7kTU Uni02EK cG5UYCf GC8V8CQ 7o1qq10").split()
 
 
 class IlhaProibida:
@@ -57,10 +57,18 @@ class IlhaProibida:
         """ Montar o tabuleiro em forma de diamante.
         
         """
-        info_terrenos = [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL] * 9
+        from random import shuffle
+        tafv = [None]*16+TAFV*2
+        info_terrenos = it = [Ter(nome=NOMES.pop(0), imagem=LINKS.pop(0),
+        tafv=tafv.pop() for _ in range(24)]
+        # como introduzir os elementos no info_terrenos?
+        # Agora info_terrenos é uma lista de Ter -> Como criar?
+        #info_terrenos = [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL] * 9
+        shuffle(info_terrenos)
+        # Cada terreno realmente criado "puxa" um terreno da lista de "Ter's
         self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
-                                 posx=((px % 6) + int(abs(2.5 - px // 6))), local=lc, ilha=self)
-                         for px, lc in enumerate(info_terrenos) if px % 6 < 6 - int(abs(2.5 - px // 6)) * 2]
+                                 posx=((px % 6) + int(abs(2.5 - px // 6))), local=it.pop(0), ilha=self)
+                         for px in range(36) if px % 6 < 6 - int(abs(2.5 - px // 6)) * 2]
         self.terrenos[4].afundar()
 
     def desocupa_e_vai_para(self, terreno_destino):
@@ -86,9 +94,16 @@ class Terreno:
     :param ilha: Referência ao tabuleiro.
     """
 
-    def __init__(self, local, posx, posy, cena, ilha):
-        self.local = Elemento(local, x=posx * 110 + 10, y=posy * 110 + 50, w=100, h=100,
+    def __init__(self, local: Ter, posx, posy, cena, ilha):
+        #img = local.imagem
+        #self.local = Elemento(img
+        img = f"https://imgur.com/{local.imagem}.jpg"
+        self.local = Elemento(img, x=posx * 110 + 10, y=posy * 110 + 50, w=100, h=100,
                               cena=cena)
+        estilo = {'background-color': 'slategray', 'color': 'white'}
+        letreiro = Elemento("", w=100, h=20, style=estilo, cena=self.local)
+        letreiro.elt.text = local.nome
+        #tafv = Elemento(local.tafv, ....)
         self.peao, self.ilha = None, ilha
         self.posx, self.posy = posx, posy
         self.local.vai = self.vai
@@ -105,7 +120,9 @@ class Terreno:
         def contiguos(origem, destino):
             if not origem:
                 return True
-            return abs(origem.posx - destino.posx) <= 1 and abs(origem.posy - destino.posy) <= 1
+            from operator import xor
+            return xor(abs(origem.posx - destino.posx) == 1,
+            abs(origem.posy - destino.posy) == 1) and not destino.afunda
 
         peao_pode_ir = contiguos(self, terreno_destino)
         # executar o movimento do peão agora que foi autorizado pelo pode ir
@@ -142,3 +159,7 @@ class Peao:
 if __name__ == "__main__":
     # IlhaProibida()
     IlhaProibida()
+    ata = Ter(nome="atalaia", imagem='imgur/xyz', tafv=None)
+    #ata.nome = "pista"
+    #print(ata.nome, ata.tafv)
+    # print([(px, int(abs(2.5-px//6))) for px in range(36)])
