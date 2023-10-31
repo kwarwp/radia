181a6dbd1e2f3036b5ed1ee0295bd53ed31cb70c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 16:49:57 -0300

    Saved pokebot/main.py @2023-10-31 19:49

--- pokebot/main.py
+++ pokebot/main.py
@@ -1,24 +1,46 @@
 # radia.pokebot.main.py
-import urllib.request
+"""Robo para classificar Pokemon.
+
+.. codeauthor:: Raphaella Freitas
+
+Changelog
+---------
+.. versionadded::    23.10
+    Usando o parser de html embutido (31).
+
+|   **Open Source Notification:** This file is part of open source program **Pokemon Robot**
+|   **Copyright © 2023  Raphaella Freitas**,
+|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
+"""
+
+import urllib
 
 from browser import document, window
+from html.parser import HTMLParser
+
+class Pokebot(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag, attrs) if tag == "table" else None
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag) if tag == "table" else None
+
+    def _handle_data(self, data):
+        print("Encountered some data  :", data)
+        
+    def url_get(self, url):
+        _fp = urllib.request.urlopen(url)
+        return _fp.read()
+
+        
 
 
-def create_script_tag(src):
-    _fp = urllib.request.urlopen(src)
-    _data = _fp.read()
-
-    _tag = document.createElement('script')
-    _tag.type = "text/javascript"
-    _tag.text = _data
-    document.get(tag='head')[0].appendChild(_tag)
-    
-
-class Pokebot:
-    def __init__(self):
-        create_script_tag("https://cdn.jsdelivr.net/npm/puppeteer@21.4.1/lib/cjs/puppeteer/puppeteer.min.js")
-        ppp = window.puppeteer
         
         
 if __name__ == "__main__":
-    Pokebot()
+    parser = Pokebot()
+    #data = parser.url_get("https://pokemon.fandom.com/pt-br/wiki/Pok%C3%A9dex_Nacional")
+    #parser.feed(data)
+    parser.feed('''<html><head><title>Test</title></head>
+<body><h1>Parse me!</h1><div id="xx"></div>
+</body></html>''')
