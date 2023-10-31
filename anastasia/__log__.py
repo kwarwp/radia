d15e6b248cd1ca828636e47d94f811887ca5e5c5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 12:15:55 -0300

    Saved anastasia/__log__.py @2023-10-31 15:15

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -2479,3 +2479,19 @@ SyntaxError: invalid syntax
     ata = Ter(nome= NOMES[1], imagem= LINK[1], tafv=TFAVS[1]) #instancia com argumentos de Ter
 NameError: name 'LINK' is not defined
 '''},
+{'date': 'Tue Oct 31 2023 12:15:35.834 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 70
+    ata = Ter(nome= NOMES[1], imagem= LINK[1], tafv=TFAVS[1]) #instancia com argumentos de Ter
+NameError: name 'LINK' is not defined
+'''},
