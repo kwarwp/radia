224e508a80e4ca9bde9ca38a3b8ae25a0b519d3c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 11:51:51 -0300

    Saved sarah/__log__.py @2023-10-31 14:51

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -495,3 +495,42 @@ AttributeError: 'str' object has no attribute 'afundar'
     self.Ter[10].afundar()
 AttributeError: 'IlhaProibida' object has no attribute 'Ter'
 '''},
+{'date': 'Tue Oct 31 2023 11:14:58.584 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 133
+    IlhaProibida()
+  module <module> line 25
+    self.monta_tabuleiro_oceano()
+  module <module> line 34
+    from random import range
+ImportError: cannot import name 'range'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 133
+    IlhaProibida()
+  module <module> line 25
+    self.monta_tabuleiro_oceano()
+  module <module> line 34
+    from random import range
+'''},
