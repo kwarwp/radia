1950cda50398ad2ea2b234ed55d53cb8bcb5fb67
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 12:30:50 -0300

    Saved grace/__log__.py @2023-10-31 15:30

--- grace/__log__.py
+++ grace/__log__.py
@@ -797,3 +797,23 @@ SyntaxError: non-keyword arg after keyword arg
                                     ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Tue Oct 31 2023 12:30:49.492 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 162
+    IlhaProibida()
+  module <module> line 52
+    self.monta_tabuleiro_oceano()
+  module <module> line 61
+    tafv = [None]*16+TAFV*2
+NameError: name 'TAFV' is not defined
+'''},
