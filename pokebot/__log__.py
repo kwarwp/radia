d22a7e84c0b2a7707e793a0322f33276d7aecec5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 17:08:10 -0300

    Saved pokebot/__log__.py @2023-10-31 20:08

--- pokebot/__log__.py
+++ pokebot/__log__.py
@@ -88,3 +88,21 @@ Exception: <NetworkError: A network error occurred.>
     parser = Pokebot()
 TypeError: __init__() missing 1 positional argument: url
 '''},
+{'date': 'Tue Oct 31 2023 17:08:09.569 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    parser.feed('''<html><head><title>Test</title></head>
+  module html.parser line 144
+    self. rawdata=self. rawdata+data
+AttributeError: 'Pokebot' object has no attribute 'rawdata'
+'''},
