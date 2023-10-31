fc94b817a3f7c3bbf5aa1fda09d28002aea19cdd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 11:36:38 -0300

    Saved tracy/__log__.py @2023-10-31 14:36

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -296,3 +296,23 @@ SyntaxError
 File "anastasia.main", line 191
     ata = Ter(nome="atalaia", imagem'imgur/xyz', tafv=none) #instancia com argumentos de Ter
 '''},
+{'date': 'Tue Oct 31 2023 11:36:37.409 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    from anastasia import main as amain
+ImportError: cannot import name 'main'
+
+SyntaxError
+File "anastasia.main", line 191
+    ata = Ter(nome="atalaia", imagem'imgur/xyz', tafv=none) #instancia com argumentos de Ter
+'''},
