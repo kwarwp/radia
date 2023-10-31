5c8a8313c82e1e65e73036673bab61f6b194d555
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 11:42:14 -0300

    Saved sarah/__log__.py @2023-10-31 14:42

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -415,3 +415,23 @@ Exception: <TypeError: self is undefined>
     Ter.insert = info_terrenos
 AttributeError: 'list' object attribute 'insert' is read-only
 '''},
+{'date': 'Tue Oct 31 2023 11:05:21.713 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 131
+    IlhaProibida()
+  module <module> line 25
+    self.monta_tabuleiro_oceano()
+  module <module> line 37
+    listater.append(info_terrenos)
+NameError: name 'listater' is not defined
+'''},
