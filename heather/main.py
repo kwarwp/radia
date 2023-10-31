7db884fcfa404d75a14b0ab81e2afabdccbdf1f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 12:00:54 -0300

    Saved heather/main.py @2023-10-31 15:00

--- heather/main.py
+++ heather/main.py
@@ -1,12 +1,22 @@
-# radia.heather.main.py
+# radia.roxanne.main.py
 from _spy.vitollino.main import Cena, Elemento, STYLE
-
+from collections import namedtuple
+Ter = namedtuple("Ter", "nome imagem tafv")
 STYLE["width"] = 800
 STYLE["height"] = "600px"
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
-PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
-PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
 PAWN = "https://imgur.com/zO3kiRp.png"
+
+TFAVS = "KXZXTei LK4p1xG rUNsKEH qp5Zbn8".split()
+
+NOMES = ("PISTA_POUSO PORTAO_BRONZE PALACIO_CORAL VALE_TENEBROSO PORTAO_OURO PORTAO_PRATA PORTAO_COBRE "
+"PORTAO_FERRO ATALAIA JARDIM_SUSSUROS JARDIM_UIVOS TEMPLO_SOL "
+"TEMPLO_LUA CAVERNA_LAVA CAVERNA_SOMBRAS OBSERVATORIO PANTANO_BRUMAS ROCHA_FANTASMA "
+"PALACIO_MARES PENEDO_BALDIO BOSQUE_CARMESIM DUNAS_ENGANO PONTE_SUSPENSA LAGOA_PERDIDA").split()
+LINKS = ("CU3TLYh BL6lB7H tLDbzd2 OZE1myn J6ow4jR v0g7eGm 45aU3nf "
+"yKU6ngz sdJ4W5O pjVcyoy ZNuPWqZ O0OSVFt "
+"J160xpm 2j1IAyf b4xtltc E9MflTP NDioDZg TCmLjeT "
+"rYxQaTa MvN7kTU Uni02EK cG5UYCf GC8V8CQ 7o1qq10").split()
 
 
 class IlhaProibida:
@@ -28,9 +38,15 @@ class IlhaProibida:
         """ Montar o tabuleiro em forma de diamante.
         
         """
-        info_terrenos = [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL] * 9
+        from random import shuffle
+        # Agora info_terrenos será uma lista de Ter -> Como criar?
+        info_terrenos = []
+        for terreno, link in zip(NOMES, LINKS):
+            info_terrenos.append(("https://imgur.com/" + link, terreno))
+        shuffle(info_terrenos)
+        # Cada terreno realmente criado "puxa" um terreno da lista de "Ter's
         self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
-                                 posx=((px % 6) + int(abs(2.5 - px // 6))), local=lc, ilha=self)
+                                 posx=((px % 6) + int(abs(2.5 - px // 6))), local=lc[0], ilha=self)
                          for px, lc in enumerate(info_terrenos) if px % 6 < 6 - int(abs(2.5 - px // 6)) * 2]
         self.terrenos[4].afundar()
 
@@ -57,12 +73,15 @@ class Terreno:
     :param ilha: Referência ao tabuleiro.
     """
 
-    def __init__(self, local, posx, posy, cena, ilha):
+    def __init__(self, local: Ter, posx, posy, cena, ilha):
+        #img = local.imagem
+        #self.local = Elemento(img
         self.local = Elemento(local, x=posx * 110 + 10, y=posy * 110 + 50, w=100, h=100,
                               cena=cena)
-        estilo = {'background-color': '#343', 'color': 'white'}
+        estilo = {'background-color': 'slategray', 'color': 'white'}
         letreiro = Elemento("", w=100, h=20, style=estilo, cena=self.local)
-        letreiro.elt.text = "UM TERRENO"
+        letreiro.elt.text = lc[1]
+        #tafv = Elemento(local.tafv, ....)
         self.peao, self.ilha = None, ilha
         self.posx, self.posy = posx, posy
         self.local.vai = self.vai
@@ -79,8 +98,9 @@ class Terreno:
         def contiguos(origem, destino):
             if not origem:
                 return True
-            return (abs(origem.posx - destino.posx) <= 1 and
-            abs(origem.posy - destino.posy) <= 1)
+            from operator import xor
+            return xor(abs(origem.posx - destino.posx) == 1,
+            abs(origem.posy - destino.posy) == 1) and not destino.afunda
 
         peao_pode_ir = contiguos(self, terreno_destino)
         # executar o movimento do peão agora que foi autorizado pelo pode ir
@@ -117,4 +137,12 @@ class Peao:
 if __name__ == "__main__":
     # IlhaProibida()
     IlhaProibida()
+    info_terrenos = []
+    for terreno, link in zip(NOMES, LINKS):
+        info_terrenos.append(("https://imgur.com/" + link, terreno))
+    print(info_terrenos)
+    print(len(info_terrenos))
+    #ata = Ter(nome="atalaia", imagem='imgur/xyz', tafv=None)
+    #ata.nome = "pista"
+    #print(ata.nome, ata.tafv)
     # print([(px, int(abs(2.5-px//6))) for px in range(36)])
