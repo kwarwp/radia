d87a7898f8b6e65a9a0114a83923d7e646ca9159
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 11:53:00 -0300

    Saved danae/__log__.py @2023-10-31 14:52

--- danae/__log__.py
+++ danae/__log__.py
@@ -493,3 +493,23 @@ NameError: name 'info_terrenos' is not defined
     info_terrenos = [Ter(nome=NOMES[px], imagem=local, tafv=None) for px, local in enumerate(info_terrenos)]
 NameError: free variable 'info_terrenos' referenced before assignment in enclosing scope
 '''},
+{'date': 'Tue Oct 31 2023 11:52:57.309 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 157
+    IlhaProibida()
+  module <module> line 52
+    self.monta_tabuleiro_oceano()
+  module <module> line 62
+    info_terrenos = [Ter(nome=NOMES[px], imagem=local, tafv=None) for px, local in enumerate(info_terrenos)]
+NameError: free variable 'info_terrenos' referenced before assignment in enclosing scope
+'''},
