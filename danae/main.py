35b284adaa7dfad95a83ffb5fa0af1305e1a5e88
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 11:08:16 -0300

    Saved danae/main.py @2023-10-31 14:08

--- danae/main.py
+++ danae/main.py
@@ -47,10 +47,10 @@ class IlhaProibida:
         """ Montar o tabuleiro em forma de diamante.
         
         """
-        info_terrenos = [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL] * 9
         self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
                                  posx=((px % 6) + int(abs(2.5 - px // 6))), local=lc, ilha=self)
-                         for px, lc in enumerate(info_terrenos) if px % 6 < 6 - int(abs(2.5 - px // 6)) * 2]
+                         for px, lc in enumerate(info_terrenos) if 6-int(abs(2.5-px//6)*2) > px % 6]
+        
         self.terrenos[4].afundar()
 
     def desocupa_e_vai_para(self, terreno_destino):
