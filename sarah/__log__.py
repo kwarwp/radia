c4a8ce4c487877555bb21dcaf49bd41e6c429256
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 11:55:43 -0300

    Saved sarah/__log__.py @2023-10-31 14:55

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -613,3 +613,23 @@ AttributeError: 'function' object has no attribute 'randint'
     self.terrenos[random.randint(1,4)].afundar()
 AttributeError: 'function' object has no attribute 'randint'
 '''},
+{'date': 'Tue Oct 31 2023 11:18:51.266 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 132
+    IlhaProibida()
+  module <module> line 25
+    self.monta_tabuleiro_oceano()
+  module <module> line 35
+    sortea = random.randint(1,35)
+AttributeError: 'function' object has no attribute 'randint'
+'''},
