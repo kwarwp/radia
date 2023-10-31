179be9e25f1e85159ec6649da44a3f4a685aaa30
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 11:25:45 -0300

    Saved grace/__log__.py @2023-10-31 14:25

--- grace/__log__.py
+++ grace/__log__.py
@@ -662,3 +662,23 @@ NameError: name 'oceano' is not defined
     info_terrenos[[(PORTAO_BRONZE, "Portão Bronze")]]
 TypeError: list indices must be integer, not list
 '''},
+{'date': 'Tue Oct 31 2023 11:25:44.818 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 153
+    IlhaProibida()
+  module <module> line 42
+    self.monta_tabuleiro_oceano()
+  module <module> line 58
+    info_terrenos[[(PORTAO_BRONZE, "Portão Bronze")]]
+TypeError: list indices must be integer, not list
+'''},
