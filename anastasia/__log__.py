a5aa52aa0e86b6f684e61b3eee52ae37ad3368d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 12:41:37 -0300

    Saved anastasia/__log__.py @2023-10-31 15:41

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -2596,3 +2596,23 @@ NameError: name 'suffle' is not defined
     suffle(info_terrenos)
 NameError: name 'suffle' is not defined
 '''},
+{'date': 'Tue Oct 31 2023 12:41:18.594 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 208
+    IlhaProibida()
+  module <module> line 87
+    self.monta_tabuleiro_oceano()
+  module <module> line 101
+    suffle(info_terrenos)
+NameError: name 'suffle' is not defined
+'''},
