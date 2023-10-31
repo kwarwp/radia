be6b12e519cff493ea9a676e06b37f2a192a046b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 10:23:54 -0300

    Saved sarah/__log__.py @2023-10-31 13:23

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -312,3 +312,10 @@ SyntaxError: invalid syntax
                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 31 2023 09:47:01.49 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 116
+  def verifica:
+               ^
+SyntaxError: invalid syntax
+'''},
