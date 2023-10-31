d4f493d28cc17deb82c01d2fb1644befc3c05990
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 15:47:23 -0300

    Saved pokebot/__log__.py @2023-10-31 18:47

--- pokebot/__log__.py
+++ pokebot/__log__.py
@@ -24,3 +24,10 @@ AttributeError: no attribute puppeteer for [object Window]
                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 31 2023 15:47:23.473 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 32
+  with urllib.request.urlopen(url) as _fp
+                                          ^
+SyntaxError: invalid syntax
+'''},
