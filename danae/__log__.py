bafcdacb477e4a903331be22eaec68ec9f435d86
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 10:55:24 -0300

    Saved danae/__log__.py @2023-10-31 13:55

--- danae/__log__.py
+++ danae/__log__.py
@@ -323,3 +323,25 @@ SyntaxError: invalid syntax
     letreiro.elt.text = NOMES_TERRENOS[posx + posy * 6]  # Aqui alteramos para usar a lista
 IndexError: list index out of range
 '''},
+{'date': 'Tue Oct 31 2023 10:55:21.972 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 155
+    IlhaProibida()
+  module <module> line 42
+    self.monta_tabuleiro_oceano()
+  module <module> line 51
+    self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
+  module <module> line 92
+    letreiro.elt.text = NOMES_TERRENOS[posx + posy * 6]  # Aqui alteramos para usar a lista
+IndexError: list index out of range
+'''},
