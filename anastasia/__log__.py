4e5cd88b2d65c245ce4aaefa236a87ed6ccf6dd4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 10:30:39 -0300

    Saved anastasia/__log__.py @2023-10-31 13:30

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -2408,3 +2408,23 @@ TypeError: __init__ missing 1 positional argument: 'ilha'
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Oct 31 2023 10:30:20.538 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 181
+    IlhaProibida()
+  module <module> line 74
+    self.peao.mover(self.terrenos[0])
+  module <module> line 164
+    self.terreno.desocupa_e_vai(terreno_destino) 
+AttributeError: 'IlhaProibida' object has no attribute 'desocupa_e_vai'
+'''},
