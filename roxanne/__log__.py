e1990aae97e3a981951f69ced05ed7423e32848b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 12:32:47 -0300

    Saved roxanne/__log__.py @2023-10-31 15:32

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -568,3 +568,23 @@ SyntaxError: invalid syntax
                                                                                                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 31 2023 12:32:47.108 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 161
+    IlhaProibida()
+  module <module> line 52
+    self.monta_tabuleiro_oceano()
+  module <module> line 61
+    tafv = [None]*16+TAFV*2
+NameError: name 'TAFV' is not defined
+'''},
