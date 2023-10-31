fd97ac9d804baa07a16ca806df3321eafaf4ed1b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 12:44:57 -0300

    Saved grace/main.py @2023-10-31 15:44

--- grace/main.py
+++ grace/main.py
@@ -107,7 +107,7 @@ class Terreno:
         if local.tafv:
             self.tafv = Elemento(
                 f"https://imgur.com/{local.tafv}.jpg", 
-                x=posx * 110 + 80, y=posy * 110 + 100, w=50, h=50,
+                x=posx * 110 + 60, y=posy * 110 + 100, w=50, h=50,
                 cena=cena
             )
         self.peao, self.ilha = None, ilha
