8e5bc975b325dac2238d81c989f76457739ba868
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 11:39:55 -0300

    Saved sarah/__log__.py @2023-10-31 14:39

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -395,3 +395,23 @@ Exception: <TypeError: self is undefined>
 'error': '''
 Exception: <TypeError: self is undefined>
 '''},
+{'date': 'Tue Oct 31 2023 11:03:02.968 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 131
+    IlhaProibida()
+  module <module> line 25
+    self.monta_tabuleiro_oceano()
+  module <module> line 37
+    Ter.insert = info_terrenos
+AttributeError: 'list' object attribute 'insert' is read-only
+'''},
