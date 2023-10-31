e817942aede821345557faf89314ccd8c9f9726d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 10:18:48 -0300

    Saved grace/__log__.py @2023-10-31 13:18

--- grace/__log__.py
+++ grace/__log__.py
@@ -487,3 +487,27 @@ NameError: name 'oceano' is not defined
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Oct 31 2023 10:18:47.493 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 141
+    IlhaProibida()
+  module <module> line 44
+    self.peao.mover(self.terrenos[0])
+  module <module> line 136
+    self.terreno.desocupa_e_vai_para(terreno_destino)
+  module <module> line 57
+    self.peao.move(terreno_destino)
+  module <module> line 129
+    if not self.terreno == None and not self.terreno.peao == Null:
+NameError: name 'Null' is not defined
+'''},
