3c897ae206ab2fc1eeac3c8ffabe14b5e62a0355
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 12:12:33 -0300

    Saved tracy/__log__.py @2023-10-31 15:12

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -392,3 +392,10 @@ SyntaxError: EOL while scanning string literal
                                            ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Tue Oct 31 2023 12:12:32.475 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 23
+  Ter = namedtuple("Ter", "nome imagem tafv
+                                           ^
+SyntaxError: EOL while scanning string literal
+'''},
