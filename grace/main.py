521d79d1b6a28db1f3618b6ce85d8a1b30b0ce4e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 10:10:44 -0300

    Saved grace/main.py @2023-10-31 13:10

--- grace/main.py
+++ grace/main.py
@@ -126,7 +126,8 @@ class Peao:
         self.ilha = ilha
 
     def move(self, terreno):
-        self.terreno.peao = None
+        print(self.terreno.peao)
+        #self.terreno.peao = None
         self.terreno = terreno
         terreno.peao = self
         self.peao.x, self.peao.y = terreno.posx * 110 + 10, terreno.posy * 110 + 50
