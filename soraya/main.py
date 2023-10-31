2bfd12e377e44c9167b9de802d2f309f7d6ffc82
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-30 23:22:57 -0300

    Saved soraya/main.py @2023-10-31 02:22

--- soraya/main.py
+++ soraya/main.py
@@ -20,7 +20,7 @@ Changelog
 
 
 from _spy.vitollino.main import Cena, Elemento, STYLE
-#from anastasia import main as amain
+
 STYLE["width"] = 800
 STYLE["height"] = "600px"
 IMAGEM = "https://imgur.com/gVHmY2v.jpg"
@@ -36,34 +36,27 @@ class IlhaProibida:
         Locais onde os peões podem ficar.
         
     """
+
     def __init__(self):
-        self.oceano = oceano = Cena(IMAGEM).vai()
-        #self.terreno = Terreno(PORTAO_BRONZE, posx=10, posy=50,
-        #cena=oceano)
-        #self.terreno1 = Terreno(PALACIO_CORAL, posx=120, posy=50, cena=oceano)
-        #info_terrenos= [(10, PORTAO_BRONZE), (120, PALACIO_CORAL), (230, PORTAO_BRONZE)]
+        self.oceano = Cena(IMAGEM).vai()
         self.terrenos = []
         self.monta_tabuleiro_oceano()
         self.peao = Peao(self)
-        self.terrenos[1].ocupa(self.peao)
-        
+        self.peao.mover(self.terrenos[0])
+
     def monta_tabuleiro_oceano(self):
         """ Montar o tabuleiro em forma de diamante.
         
         """
-        matriz = [ [0,0,1,1,0,0], [0,1,1,1,1,0], [1,1,1,1,1,1], [1,1,1,1,1,1], [0,1,1,1,1,0], [0,0,1,1,0,0]]
-        info_terrenos= [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL] * 9
-        py=50;
-        px=1;
-        for i in range(6):
-            for j in range(6):
-                if matriz[i][j] == 1:
-                    self.terrenos = [Terreno(local=info_terrenos[i*j], posy=py, posx=px*110+10,cena=oceano)]
-                px = px+1
-            py = py + 110
-            px = 1
-                    
-        
+        info_terrenos = [PORTAO_BRONZE, PALACIO_CORAL, PORTAO_BRONZE, PALACIO_CORAL] * 9
+        self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
+                                 posx=((px % 6) + int(abs(2.5 - px // 6))), local=lc, ilha=self)
+                         for px, lc in enumerate(info_terrenos) if px % 6 < 6 - int(abs(2.5 - px // 6)) * 2]
+        self.terrenos[4].afundar()
+
+    def desocupa_e_vai_para(self, terreno_destino):
+        self.peao.move(terreno_destino)
+
     def direita(self, terreno):
         """ Move o peão para a direita.
         
@@ -71,49 +64,73 @@ class IlhaProibida:
         :return: O terreno onde o peão vai
         """
         aqui = self.terrenos.index(terreno)
-        return self.terrenos[aqui+1]
+        return self.terrenos[aqui + 1]
+
 
 class Terreno:
-    def __init__(self, local, posx, posy, cena):
-        self.local = Elemento(local, x=posx, y=posy, w=100, h= 100, cena=cena, vai=self.ocupa)
-        self.peao = None
+    """ Local onde um peão pode ficar.
+
+    :param local: Imagem do terreno
+    :param posx: Coordenada x do terreno.
+    :param posy: Coordenada y do terreno.
+    :param cena: Cena do local.
+    :param ilha: Referência ao tabuleiro.
+    """
+
+    def __init__(self, local, posx, posy, cena, ilha):
+        self.local = Elemento(local, x=posx * 110 + 10, y=posy * 110 + 50, w=100, h=100,
+                              cena=cena)
+        self.peao, self.ilha = None, ilha
         self.posx, self.posy = posx, posy
-        
+        self.local.vai = self.vai
+        self.afunda = False
+
+    def vai(self, _=0):
+        self.ilha.peao.mover(self)
+
+    def afundar(self):
+        self.afunda = True
+        self.local.o = 0.2
+
+    def desocupa_e_vai_para(self, terreno_destino):
+        def contiguos(origem, destino):
+            if not origem:
+                return True
+            return abs(origem.posx - destino.posx) <= 1 and abs(origem.posy - destino.posy) <= 1
+
+        peao_pode_ir = contiguos(self, terreno_destino)
+        # executar o movimento do peão agora que foi autorizado pelo pode ir
+        self.peao.move(terreno_destino) if peao_pode_ir else None
+
     def ocupa(self, peao):
         self.peao = peao
         peao.mover(self.posx, self)
 
+
 class Peao:
     """ Marcador usado para definir a posição do jogador nos terrenos.
+        
+        :param ilha: Referência ao tabuleiro.
     """
+
     def __init__(self, ilha):
-        self.peao = Elemento(PAWN, x=20, y=70, w=80, h= 80,
-        cena=ilha.oceano, vai=self.move)
-        self.terreno = None
+        """
+        """
+        self.peao = Elemento(PAWN, x=20, y=70, w=80, h=80,
+                             cena=ilha.oceano)
+        self.terreno = ilha  # era None, mas o peão agora nasce na ilha
         self.ilha = ilha
-        #self.peao.vai = self.move
-        
-    def move_(self, ev=None):  # Corrigir: não está condizente!
-        terreno_destino = self.ilha.direita(0) #(self.terreno)
-        self.peao.y = 300
-        
-    def move(self, ev=None):  # Corrigir: não está condizente!
-        terreno_destino = self.ilha.direita(self.terreno)
-        #self.peao.x = 170
-        terreno_destino.ocupa(self)        
-    def mover(self, x, terreno):
+
+    def move(self, terreno):  # Corrigir: não está condizente!
         self.terreno = terreno
-        self.peao.x = terreno.posx
+        terreno.peao = self
+        self.peao.x, self.peao.y = terreno.posx * 110 + 10, terreno.posy * 110 + 50
 
-        
-if __name__ == "__main__": 
-    #print((amain.__name__))
-    ##import antigravity as ag
-    #from __future__ import braces
-    import __phello__
-    #ag.fly()
-    
+    def mover(self, terreno_destino):
+        self.terreno.desocupa_e_vai_para(terreno_destino)
+
+
+if __name__ == "__main__":
+    # IlhaProibida()
     IlhaProibida()
-    #IlhaProibida()
-
-
+    # print([(px, int(abs(2.5-px//6))) for px in range(36)])
