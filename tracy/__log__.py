b83d951f25dd7be0df06533cfc7f8341c6e11809
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 10:17:53 -0300

    Saved tracy/__log__.py @2023-10-31 13:17

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -242,3 +242,21 @@ AttributeError: 'IlhaProibida' object has no attribute 'monta_tabuleiro_oceano'
     self.monta_tabuleiro_oceano()
 AttributeError: 'IlhaProibida' object has no attribute 'monta_tabuleiro_oceano'
 '''},
+{'date': 'Tue Oct 31 2023 10:17:51.530 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 116
+    IlhaProibida()
+  module <module> line 47
+    self.monta_tabuleiro_oceano()
+AttributeError: 'IlhaProibida' object has no attribute 'monta_tabuleiro_oceano'
+'''},
