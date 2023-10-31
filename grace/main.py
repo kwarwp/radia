b7998def3093469da3e5dd633ed45182ccd44149
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-31 11:40:31 -0300

    Saved grace/main.py @2023-10-31 14:40

--- grace/main.py
+++ grace/main.py
@@ -47,7 +47,7 @@ class IlhaProibida:
         """ Montar o tabuleiro em forma de diamante.
         
         """
-        info_terrenos = [
+        info_terrenoseeee = [
             [(PORTAO_BRONZE, "Portão Bronze"), (PALACIO_CORAL, "Palacio Coral")],
             [(PORTAO_BRONZE, "Portão Bronze"), (PALACIO_CORAL, "Palacio Coral"), (PORTAO_BRONZE, "Portão Bronze"), (PALACIO_CORAL, "Palacio Coral")],
             [(PORTAO_BRONZE, "Portão Bronze"), (PALACIO_CORAL, "Palacio Coral"), (PORTAO_BRONZE, "Portão Bronze"), (PALACIO_CORAL, "Palacio Coral"), (PORTAO_BRONZE, "Portão Bronze"), (PALACIO_CORAL, "Palacio Coral")],
@@ -61,7 +61,7 @@ class IlhaProibida:
                 posx=(6-len(row))*55 + px*110+10, posy= py*110 + 10,
                 local=lc[0], name=lc[1]
             )
-            for py, row in enumerate(info_terrenos) for px, lc in enumerate(row)
+            for py, row in enumerate(info_terrenoseeee) for px, lc in enumerate(row)
         ]
         
         self.terrenos[4].afundar()
