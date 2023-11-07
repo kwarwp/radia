9c9368434481442640f194d7d2ea2d5cb5a8101b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 10:55:24 -0300

    Saved vento/main.py @2023-11-07 13:55

--- vento/main.py
+++ vento/main.py
@@ -1,16 +1,3 @@
-Changelog
----------
-.. versionadded::    23.10
-    Classes Ilha, Terreno, Peao (10).
-    
-.. versionadded::    23.09
-    Versão Inicial (26).
-
-|   **Open Source Notification:** This file is part of open source program **Ilha Proibida**
-|   **Copyright © 2023  Carlo Oliveira** <carlo@nce.ufrj.br>,
-|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
-|   `Labase <http://labase.selfip.org/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
-"""
 
 from _spy.vitollino.main import Cena, Elemento, STYLE
 from collections import namedtuple
