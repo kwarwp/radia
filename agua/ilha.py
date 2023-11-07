f343c9c0c3cd7b25e4bada0bfa2c8cceb5a941af
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 12:11:03 -0300

    Saved agua/ilha.py @2023-11-07 15:11

--- agua/ilha.py
+++ agua/ilha.py
@@ -30,7 +30,29 @@ Changelog
 
 class IlhaProibida:
     def __init__(self):
+        baralho_tesouro = bt = list("tafv" * 5 +"dd" + "hhh")
+        baralho_alaga = ba = "eee" 
+        self.cartas_tesouro = [CartaTesouro(face=carta) for carta in bt]
+        # recursos do jogador para relizar capturas, drenagem e movimentos
+        self.cartas_tesouro += [CartaAlagamento(face=carta) for carta in ba]
+        # acrescido de um conjunto de cartas que causam uma enchente
+        self.cartas_inunda = list(range(24))
+        # abaixa um terreno que fica alagado ou afunda no oceano
         print("Bemvindos à Ilha Poibida - montagem do tabuleiro")
+
+
+class CartaTesouro:
+    def __init__(self, face):
+        self.face = face
+        print("Bemvindos à Ilha Poibida - montagem do tabuleiro")
+
+
+class CartaAlagamento(CartaTesouro):
+    def __init__(self, face):
+        super().__init__(self, face)
+        print("Bemvindos à Ilha Poibida - montagem do tabuleiro")
+        
+        
         
         
 if __name__ == "__main__":
