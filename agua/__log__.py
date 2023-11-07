d2a47d17179b5a167358a1271b2b5c39538175ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 19:12:34 -0300

    Saved agua/__log__.py @2023-11-07 22:12

--- agua/__log__.py
+++ agua/__log__.py
@@ -55,3 +55,23 @@ SyntaxError: invalid syntax
     super().__init__(self, face)
 TypeError: __init__() takes 2 positional arguments but more were given
 '''},
+{'date': 'Tue Nov 07 2023 19:12:33.931 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    main()
+  module <module> line 31
+    Ilha()
+  module agua.ilha line 39
+    LG.log(0, "Bem vindos à Ilha Proibida - montagem do tabuleiro")
+AttributeError: 'module' object has no attribute 'log'
+'''},
