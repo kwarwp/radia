cb6adeb6acc401c1cb9ea57bea0462dabcec71d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 11:23:36 -0300

    Created fogo/ilha.py @2023-11-07 14:23

---
+++ fogo/ilha.py
@@ -0,0 +1,20 @@
+# radia.fogo.ilha.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    23.11
+        Descreva o que você adicionou no código.
+
+"""
+
+class IlhaProibida:
+    def __init__(self):
+        print("Bemvindos à Ilha Poibida - montagem do tabuleiro")
+        
+        
+if __name__ == "__main__":
+    IlhaProibida()
