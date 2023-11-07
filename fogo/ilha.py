98fcc332626ed60622b585f31dac71d7e944cd58
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 13:07:59 -0300

    Saved fogo/ilha.py @2023-11-07 16:07

--- fogo/ilha.py
+++ fogo/ilha.py
@@ -32,7 +32,6 @@ class CartaTesouro:
 class CartaAlagamento(CartaTesouro):
     def __init__(self, face):
         super(self, face)
-        print("Bemvindos à Ilha Poibida - montagem do tabuleiro")
         
         
         
