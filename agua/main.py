c8bebe25516388731d2d1dee5b977a6dcd4c8a94
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 10:59:04 -0300

    Created agua/main.py @2023-11-07 13:59

---
+++ agua/main.py
@@ -0,0 +1,26 @@
+# radia.agua.main.py
+"""Página de entrada do jogo Ilha Proibida.
+
+LOG - http://bit.ly/Dev_Agile_23
+
+EQUIPE Água
+
+.. codeauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
+.. codeauthor:: Julia <julia@gmail.com>
+
+Changelog
+---------
+.. versionadded::    23.11
+    Divisão de equipes (07).
+    
+.. versionadded::    23.10
+    Classes Ilha, Terreno, Peao (10).
+    
+.. versionadded::    23.09
+    Versão Inicial (26).
+
+|   **Open Source Notification:** This file is part of open source program **Ilha Proibida**
+|   **Copyright © 2023  Carlo Oliveira** <carlo@nce.ufrj.br>,
+|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
+|   `Labase <http://labase.selfip.org/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
+"""
