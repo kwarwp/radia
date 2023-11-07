e1760ec094ff5b06bf07e2b2226c41414263f34e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 13:06:13 -0300

    Saved fogo/__log__.py @2023-11-07 16:06

--- fogo/__log__.py
+++ fogo/__log__.py
@@ -140,3 +140,54 @@ Traceback (most recent call last):
     (self.dono.nome)
 AttributeError: 'Jogador' object has no attribute 'nome'
 '''},
+{'date': 'Tue Nov 07 2023 13:06:10.587 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Bemvindos à Ilha Poibida - montagem do tabuleiro
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    Jogador()
+  module fogo.jogador line 28
+    self.mao = MaoJogador(dono=self)
+  module fogo.jogador line 37
+    print(f"A mão do {self.dono.nome} possui {len(self.cartas)} cartas")
+  module <module> line 1
+    (self.dono.nome)
+AttributeError: 'Jogador' object has no attribute 'nome'
+'''},
