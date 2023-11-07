35570091657a6966d8526dba8cf7020ecbd5968d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 11:31:58 -0300

    Created agua/jogador.py @2023-11-07 14:31

---
+++ agua/jogador.py
@@ -0,0 +1,38 @@
+# radia.agua.jogador.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""Classes Relacionadas com o Jogador.
+
+LOG - http://bit.ly/Dev_Agile_23
+
+EQUIPE Água
+
+.. codeauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
+.. codeauthor:: Julia <julia@gmail.com>
+
+Changelog
+---------
+.. versionadded::    23.11
+    Classe Jogador e MaoJogador (07).
+
+|   **Open Source Notification:** This file is part of open source program **Ilha Proibida**
+|   **Copyright © 2023  Carlo Oliveira** <carlo@nce.ufrj.br>,
+|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
+|   `Labase <http://labase.selfip.org/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
+"""
+
+
+class Jogador:
+    def __init__(self):
+        print("Jogador se apresentando")
+
+
+class MaoJogador:
+    def __init__(self):
+        self.cartas = []
+        print(f"A mão possui {len(self.cartas)} cartas")
+        
+        
+if __name__ == "__main__":
+    #IlhaProibida()
+    Jogador()
+    MaoJogador()
