0887daf9f28f9421fc36451e252692374df188a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 12:41:20 -0300

    Created vento/cartas.py @2023-11-07 15:41

---
+++ vento/cartas.py
@@ -0,0 +1,15 @@
+# radia.vento.cartas.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Glaucio de Sousa Santos <glaucio75@ufrj.br>
+.. codeauthor:: Bruno Lemos Barroso <blemosb@gmail.com>
+.. codeauthor:: Filipe Tanus Marçal <filipetanusmarcal2004@gmail.com>
+.. codeauthor:: José André Bernardo <josebernardo@tecnico.ulisboa.pt>
+
+Changelog
+---------
+.. versionadded::    23.11
+        Descreva o que você adicionou no código.
+
+"""
