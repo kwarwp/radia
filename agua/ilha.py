8c291a1157bb9a5784151a8f30ee02458e926871
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 11:15:34 -0300

    Created agua/ilha.py @2023-11-07 14:15

---
+++ agua/ilha.py
@@ -0,0 +1,37 @@
+# radia.agua.ilha.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+# radia.agua.main.py
+"""A Ilha é o módulo de estruturação e início do jogo.
+
+LOG - http://bit.ly/Dev_Agile_23
+
+EQUIPE Água
+
+.. codeauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
+.. codeauthor:: Julia <julia@gmail.com>
+
+Changelog
+---------
+.. versionadded::    23.11
+    Divisão de equipes (07).
+    
+.. versionadded::    23.10
+    Classes Ilha, Terreno, Peao (10).
+    
+.. versionadded::    23.09
+    Versão Inicial (26).
+
+|   **Open Source Notification:** This file is part of open source program **Ilha Proibida**
+|   **Copyright © 2023  Carlo Oliveira** <carlo@nce.ufrj.br>,
+|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
+|   `Labase <http://labase.selfip.org/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
+"""
+
+
+class IlhaProibida:
+    def __init__(self):
+        print("Bemvindos à Ilha Poibida")
+        
+        
+if __name__ == "__main__":
+    IlhaProibida()
