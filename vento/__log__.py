d2a8792ad529649dfff880ac707c2d6e2f2f04fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 10:53:35 -0300

    Created vento/__log__.py @2023-11-07 13:53

---
+++ vento/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Nov 07 2023 09:49:25.261 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  ---------
+            ^
+SyntaxError: invalid syntax
+'''},
