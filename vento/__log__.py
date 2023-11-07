ebaa85d47946eeac59cd75646a09b4e1e5a2162e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 13:02:16 -0300

    Saved vento/__log__.py @2023-11-07 16:02

--- vento/__log__.py
+++ vento/__log__.py
@@ -70,3 +70,26 @@ Traceback (most recent call last):
     return sample(cartas,2)
 NameError: name 'sample' is not defined
 '''},
+{'date': 'Tue Nov 07 2023 11:58:06.670 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Bemvindos à Ilha Poibida - montagem do tabuleiro
+cartas tesouro: [f, f, , h, f, a, v, a, f, , a, t, v, t, a, a, v, f, v, t, v, d, d, t, h, , t, h]
+cartas de inundacao: [7, 14, 17, 15, 6, 20, 10, 12, 4, 23, 2, 9, 0, 11, 13, 22, 5, 1, 21, 16, 3, 18, 8, 19]
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    IlhaProibida()
+  module <module> line 29
+    print("cartas distribuidas:", self.distribuir_cartas_tesouro(self.cartas_tesouro))
+  module <module> line 32
+    return random.sample(cartas,2)
+AttributeError: 'function' object has no attribute 'sample'
+'''},
