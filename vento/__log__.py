913bac2195828d252f8bc528478a20843bc6ad95
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 13:01:21 -0300

    Saved vento/__log__.py @2023-11-07 16:01

--- vento/__log__.py
+++ vento/__log__.py
@@ -47,3 +47,26 @@ SyntaxError: invalid syntax
     ilha()
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Tue Nov 07 2023 11:57:11.527 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Bemvindos à Ilha Poibida - montagem do tabuleiro
+cartas tesouro: [f, v, t, h, a, f, f, , t, , v, a, a, t, , f, h, h, d, d, t, a, t, v, v, f, a, v]
+cartas de inundacao: [17, 20, 3, 16, 7, 11, 18, 13, 9, 21, 8, 10, 12, 22, 4, 5, 14, 0, 15, 23, 6, 19, 2, 1]
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    IlhaProibida()
+  module <module> line 29
+    print("cartas distribuidas:", self.distribuir_cartas_tesouro(self.cartas_tesouro))
+  module <module> line 32
+    return sample(cartas,2)
+NameError: name 'sample' is not defined
+'''},
