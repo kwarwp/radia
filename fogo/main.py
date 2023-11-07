53ab783bbe3e1b0033741cbb51fa6037977d80c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 11:19:52 -0300

    Saved fogo/main.py @2023-11-07 14:19

--- fogo/main.py
+++ fogo/main.py
@@ -28,6 +28,8 @@ Changelog
 |   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
 |   `Labase <http://labase.selfip.org/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
 """
+
+from fogo.ilha import IlhaProibida as Ilha 
 class IlhaProibida:
     def __init__(self):
         print("Bemvindos à Ilha Poibida")
