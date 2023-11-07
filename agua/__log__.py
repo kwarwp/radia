61667da145d255d019cb88697334847db5ace5da
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-11-07 12:23:10 -0300

    Saved agua/__log__.py @2023-11-07 15:23

--- agua/__log__.py
+++ agua/__log__.py
@@ -35,3 +35,23 @@ SyntaxError: invalid syntax
                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Nov 07 2023 12:23:14.884 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 63
+    IlhaProibida()
+  module <module> line 37
+    self.cartas_tesouro += [CartaAlagamento(face=carta) for carta in ba]
+  module <module> line 57
+    super().__init__(self, face)
+TypeError: __init__() takes 2 positional arguments but more were given
+'''},
