556183e7bc0e45275e45d1f4c5c280e9cd28884b
Finn Kockelke <Finn_Kockelke@gmx.net>
2023-12-05 11:21:44 -0300

    docs: add docstrings

--- fogo/ilha.py
+++ fogo/ilha.py
@@ -16,32 +16,41 @@ Changelog
 import random
 
 NOMES = ("PISTA_POUSO PORTAO_BRONZE PALACIO_CORAL VALE_TENEBROSO PORTAO_OURO PORTAO_PRATA PORTAO_COBRE "
-"PORTAO_FERRO ATALAIA JARDIM_SUSSUROS JARDIM_UIVOS TEMPLO_SOL "
-"TEMPLO_LUA CAVERNA_LAVA CAVERNA_SOMBRAS OBSERVATORIO PANTANO_BRUMAS ROCHA_FANTASMA "
-"PALACIO_MARES PENEDO_BALDIO BOSQUE_CARMESIM DUNAS_ENGANO PONTE_SUSPENSA LAGOA_PERDIDA").split()
+         "PORTAO_FERRO ATALAIA JARDIM_SUSSUROS JARDIM_UIVOS TEMPLO_SOL "
+         "TEMPLO_LUA CAVERNA_LAVA CAVERNA_SOMBRAS OBSERVATORIO PANTANO_BRUMAS ROCHA_FANTASMA "
+         "PALACIO_MARES PENEDO_BALDIO BOSQUE_CARMESIM DUNAS_ENGANO PONTE_SUSPENSA LAGOA_PERDIDA").split()
+
 
 class IlhaProibida:
     """
-    classe ilha proibida
+    Classe principal que representa o jogo Ilha Proibida.
     """
+
     def __init__(self, jogadores=None):
+        """
+        Inicializa a instância do jogo Ilha Proibida.
+
+        :param jogadores: Lista de jogadores.
+        """
         if jogadores is None:
             jogadores = []
-        baralho_tesouro = bt = list("tafv" * 5 +"dd" + "hhh")
-        baralho_alaga = ba = "eee" 
+        baralho_tesouro = bt = list("tafv" * 5 + "dd" + "hhh")
+        baralho_alaga = ba = "eee"
         self.cartas_tesouro = [CartaTesouro(face=carta) for carta in bt]
         # recursos do jogador para relizar capturas, drenagem e movimentos
         self.cartas_tesouro += [CartaAlagamento(face=carta) for carta in ba]
         # acrescido de um conjunto de cartas que causam uma enchente
         self.cartas_inunda = list(range(24))
         # abaixa um terreno que fica alagado ou afunda no oceano
-        print("Bemvindos à Ilha Poibida - montagem do tabuleiro")
+        print("Bem-vindos à Ilha Poibida - montagem do tabuleiro")
         self.terrenos = []
         self.monta_tabuleiro_oceano(jogadores)
 
     def monta_tabuleiro_oceano(self, jogadores):
-        """ Montar o tabuleiro em forma de diamante.
+        """
+        Monta o tabuleiro em forma de diamante.
 
+        :param jogadores: Lista de jogadores.
         """
         from random import shuffle
         tafv = [" "] * 15 + list("tafv") * 2
@@ -91,30 +100,57 @@ class IlhaProibida:
         tabuleiro += " " * (spaces - 1) + "|" + " " * spaces + "| "
         print(tabuleiro)
 
-        # Cada terreno realmente criado "puxa" um terreno da lista de "Ter's
-        # self.terrenos = [Terreno(cena=self.oceano, posy=px // 6,
-        #                          posx=((px % 6) + int(abs(2.5 - px // 6))), local=it.pop(0), ilha=self)
-        #                  for px in range(36) if px % 6 < 6 - int(abs(2.5 - px // 6)) * 2]
-
-
 
 class CartaTesouro:
+    """
+    Classe que representa uma carta de tesouro no jogo Ilha Proibida.
+    """
+
     def __init__(self, face):
+        """
+        Inicializa a instância da carta de tesouro.
+
+        :param face: Face da carta.
+        """
         self.face = face
 
 
 class CartaAlagamento(CartaTesouro):
+    """
+    Classe que representa uma carta de alagamento no jogo Ilha Proibida.
+    """
+
     def __init__(self, face):
+        """
+        Inicializa a instância da carta de alagamento.
+
+        :param face: Face da carta.
+        """
         super().__init__(face)
 
 
 class Terreno:
+    """
+    Classe que representa um terreno no tabuleiro do jogo Ilha Proibida.
+    """
+
     def __init__(self, nome, tafv):
+        """
+        Inicializa a instância do terreno.
+
+        :param nome: Nome do terreno.
+        :param tafv: Valor da face do terreno.
+        """
         self.nome = nome
         self.tafv = tafv
         self.jogadores = []
 
     def string_rep(self):
+        """
+        Retorna uma representação em string do terreno.
+
+        :return: String representando o terreno.
+        """
         result = self.nome + " " + self.tafv
         if len(self.jogadores) > 0:
             result += " " + ''.join([jog.nome[0] for jog in self.jogadores])
@@ -125,8 +161,13 @@ class Terreno:
         return result + " " * faltam
 
     def coloquar_jogador(self, jogador):
+        """
+        Coloca um jogador no terreno.
+
+        :param jogador: Instância do jogador a ser colocado no terreno.
+        """
         self.jogadores.append(jogador)
-        
-        
+
+
 if __name__ == "__main__":
     IlhaProibida()
