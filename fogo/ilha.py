4108b2e380e596986626277e87273d1522035d35
Finn Kockelke <Finn_Kockelke@gmx.net>
2023-12-05 21:20:03 -0300

    feat: add sunken islands

--- fogo/ilha.py
+++ fogo/ilha.py
@@ -38,17 +38,18 @@ class IlhaProibida:
         """
         if jogadores is None:
             jogadores = []
-        baralho_tesouro = bt = list("tafv" * 5 + "dd" + "hhh")
-        baralho_alaga = ba = "eee"
-        self.cartas_tesouro = [CartaTesouro(face=carta) for carta in bt]
-        # recursos do jogador para relizar capturas, drenagem e movimentos
-        self.cartas_tesouro += [CartaAlagamento(face=carta) for carta in ba]
-        # acrescido de um conjunto de cartas que causam uma enchente
-        self.cartas_inunda = list(range(24))
-        # abaixa um terreno que fica alagado ou afunda no oceano
-        print("Bem-vindos à Ilha Poibida - montagem do tabuleiro")
         self.terrenos = []
+
+        self.cartas_tesouro = [CartaTesouro(face=carta) for carta in list("tafv" * 5 + "dd" + "hhh")]
+        self.cartas_tesouro += [CartaAlagamento(face=carta) for carta in "eee"]
+
+        print("Bem-vindos à Ilha Poibida - montagem do tabuleiro")
         self.monta_tabuleiro_oceano(jogadores)
+
+        # abaixa um terreno que fica alagado ou afunda no oceano
+        self.terrenos[random.randrange(0, len(self.terrenos))].afundar()
+
+        # mostra o tabuleiro
         self.imprimir_tabuleiro_ilha()
 
     def monta_tabuleiro_oceano(self, jogadores):
@@ -164,6 +165,7 @@ class Terreno:
         self.nome = nome
         self.tafv = tafv
         self.jogadores = []
+        self.afundado = False
 
     def string_rep(self):
         """
@@ -171,7 +173,7 @@ class Terreno:
 
         :return: String representando o terreno.
         """
-        result = self.nome
+        result = self.nome.lower() if self.afundado else self.nome
         if len(self.jogadores) > 0:
             result += " " + ''.join([jog.nome[0] for jog in self.jogadores])
 
@@ -185,6 +187,9 @@ class Terreno:
         :param jogador: Instância do jogador a ser colocado no terreno.
         """
         self.jogadores.append(jogador)
+
+    def afundar(self):
+        self.afundado = True
 
 
 if __name__ == "__main__":
