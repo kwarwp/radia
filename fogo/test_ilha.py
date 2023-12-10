4948570991f8d8ceff3aa3e349a2dd4301841c30
VanessaVianna <vanmvianna@gmail.com>
2023-12-10 20:26:47 -0300

    script de teste

--- fogo/test_ilha.py
+++ fogo/test_ilha.py
@@ -1,18 +1,37 @@
-#pytest in action - test_ilha.py
-'''
-Este script define um teste unitario para a função xxxx usando a biblioteca pytest.
-Por meio do comando pytest test_pytest.py no terminal é executado o teste.
-'''
+# radia.fogo.test_ilha.py
+# __author__ Fernanda, Finn, Vanessa
+"""Página de teste do módulo ilha.py do jogo Ilha Proibida da Equipe Fogo.
 
-import pytest
+EQUIPE FOGO
+
+.. codeauthor:: Fernanda Araujo <fernandacsaraujo@gmail.com>
+.. codeauthor::  Finn Kockelke <finn_kockelke@gmx.net>
+.. codeauthor:: Vanessa M Vianna <vanmvianna@gmail.com>
+.. codeauthor:: Anderson Amorim da Silva <anderson.amorix@gmail.com>
+
+Changelog
+---------
+.. versionadded::    10.12
+   test 1
+
+|   **Open Source Notification:** This file is part of open source program **Ilha Proibida**
+|   **Copyright © 2023, Fernanda Araujo** <fernandacsaraujo@gmail.com>, Finn Kockelke** <finn_kockelke@gmx.net>, Vanessa M Vianna** <vanmvianna@gmail.com>
+|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
+|   `Labase <http://labase.selfip.org/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
+"""
+
 from fogo.ilha import monta_tabuleiro_oceano
 
 def test_monta_tabuleiro_oceano():
     '''
-    Teste unitario na funcao monta_tabuleiro_oceano
-    verificar número de cartas = 24
+    Define um Teste unitario para a funcao monta_tabuleiro_oceano do modulo fogo.ilha
+    usando a biblioteca pytest. Por meio do comando pytest test_pytest.py no terminal é executado o teste.
+    Verifica o numero_cartas em self.terrenos.
     '''
-        numero_cartas = len(terrenos)
-        assert numero_cartas == 24, f"O número de cartas sorteadas é {numero_cartas}, não 24."
+        assert self.terrenoss == 24, f"O número de cartas sorteadas é {numero_cartas}, não 24."
 
-verificar_numero_cartas(self.terrenos)
+def test_2():
+    '''
+    finn e anderson coloquem outro teste aqui
+    '''
+        assert self.terrenoss == 0
