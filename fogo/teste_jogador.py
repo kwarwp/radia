c04888896a744cdc215925e7a5b4395aefbf4ca1
Fernanda <fernandacsaraujo@gmail.com>
2023-12-12 10:44:22 -0300

    resolve conflict

--- fogo/teste_jogador.py
+++ fogo/teste_jogador.py
@@ -1,17 +1,18 @@
+# teste_jogador.py
 
 import unittest
-from fogo.ilha import IlhaProibida as
+from fogo.ilha import IlhaProibida
 from fogo.jogador import Jogador
 
 class TestIlhaProibida(unittest.TestCase):
 
     def test_numero_jogadores(self):
         # Crie uma instância de IlhaProibida com uma lista de jogadores
-        jogadores = ['Jogador1', 'Jogador2', 'Jogador3']
-        jogo = IlhaProibida(Jogador)
+        jogadores = [Jogador("Jose"), Jogador("Fernando"), Jogador("Igor"), Jogador("Vanessa")]
+        jogo = IlhaProibida(jogadores)
 
         # Verifique se o número de jogadores na instância do jogo é igual ao número de jogadores fornecidos
-        self.assertEqual(len(jogo.Jogador), len(Jogador))
+        self.assertEqual(len(jogo.terrenos[0].jogadores), len(jogadores))
 
 if __name__ == '__main__':
     unittest.main()
