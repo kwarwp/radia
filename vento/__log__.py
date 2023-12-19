29399a02fe7387f4195e33cf1368dd607eff39e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-12-19 11:35:21 -0300

    Saved vento/__log__.py @2023-12-19 14:35

--- vento/__log__.py
+++ vento/__log__.py
@@ -202,3 +202,21 @@ TypeError: 'module' object is not callable
     lista_de_artefatos.append(artefato2)
 NameError: name 'artefato2' is not defined
 '''},
+{'date': 'Tue Dec 19 2023 11:35:20.326 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 374
+    setup(numero_jogadores=4)
+  module <module> line 231
+    lista_tesouros = tesouros.copy()
+AttributeError: 'NoneType' object has no attribute 'copy'
+'''},
