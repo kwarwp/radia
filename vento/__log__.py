b9a35eb3e9c00dd339a669094717fe1a139286ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-12-19 11:34:37 -0300

    Saved vento/__log__.py @2023-12-19 14:34

--- vento/__log__.py
+++ vento/__log__.py
@@ -182,3 +182,23 @@ AttributeError: 'function' object has no attribute 'sample'
     artefato1 = Artefato("   TESOURO FOGO  ", "")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Tue Dec 19 2023 11:34:36.631 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 374
+    setup(numero_jogadores=4)
+  module <module> line 202
+    tesouros = criar_artefatos()
+  module <module> line 178
+    lista_de_artefatos.append(artefato2)
+NameError: name 'artefato2' is not defined
+'''},
