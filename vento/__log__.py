02bd42a33c378493ac4e394c43f0d07ea31ea652
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-12-23 12:28:39 -0300

    Saved vento/__log__.py @2023-12-23 15:28

--- vento/__log__.py
+++ vento/__log__.py
@@ -240,3 +240,26 @@ AttributeError: 'NoneType' object has no attribute 'copy'
     artefato1 = Artefato("   TESOURO FOGO  ", "")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Sat Dec 23 2023 12:28:39.31 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Bemvindos à Ilha Poibida - montagem do tabuleiro
+cartas tesouro: [t, f, a, a, v, , a, v, t, h, a, d, v, f, h, t, f, , f, v, d, t, h, t, a, , v, f]
+cartas de inundacao: [7, 17, 16, 10, 23, 18, 19, 0, 14, 2, 11, 6, 3, 12, 9, 15, 4, 8, 21, 13, 22, 1, 5, 20]
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 162
+    ilha()
+  module vento.cartas line 29
+    print("cartas distribuidas:", self.distribuir_cartas_tesouro(self.cartas_tesouro))
+  module vento.cartas line 32
+    return random.sample(cartas,2)
+AttributeError: 'function' object has no attribute 'sample'
+'''},
